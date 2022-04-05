85c2a41b8774e2ca8a901e7619400beea18dab6d
teawithsand <teawithsand@gmail.com>
2022-04-05 16:13:50 +0000

    Added icons to footer and contept of links to other platforms

--- assets/Component/UI/Common/Footer.tsx
+++ assets/Component/UI/Common/Footer.tsx
@@ -2,11 +2,23 @@ import * as React from "react"
 
 import style from "./footer.scss?module"
 
+import { icons } from "feather-icons"
+import { linkGithub, linkLinkedIn, linkYouTube } from "@app/Component/endponts"
+
 export default () => {
+
     return <footer className={style.footer}>
         <hr></hr>
         <div className={style.footer__left}>
-            
+            <a href={linkGithub} title="Github" className={style.footer_icon}>
+                <img src={`data:image/svg+xml;utf8,${icons.github.toSvg()}`} alt="Github" />
+            </a>
+            <a href={linkLinkedIn} title="LinkedIn" className={style.footer_icon}>
+                <img src={`data:image/svg+xml;utf8,${icons.linkedin.toSvg()}`} alt="LinkedIn" />
+            </a>
+            <a href={linkYouTube} title="YouTube" className={style.footer_icon}>
+                <img src={`data:image/svg+xml;utf8,${icons.youtube.toSvg()}`} alt="YouTube" />
+            </a>
         </div>
         <div className={style.footer__right}>
             By Teawithsand 2022 - echo date('Y');
--- assets/Component/endponts.ts
+++ assets/Component/endponts.ts
@@ -3,3 +3,7 @@ export const portfolioPath = "/portfolio"
 export const contactPath = "/contact"
 export const aboutMePath = "/about-me"
 export const homePath = "/"
+
+export const linkGithub = "https://github.com/teawithsand"
+export const linkLinkedIn = "https://github.com/teawithsand"
+export const linkYouTube = "https://github.com/teawithsand"
