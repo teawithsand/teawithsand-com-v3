32e84593bc9ff2d7a528b28436647d068e4b22b6
teawithsand <teawithsand@gmail.com>
2022-04-06 09:57:04 +0000

    Improved code display, added more languages

--- assets/Component/UI/Util/Code/Code.tsx
+++ assets/Component/UI/Util/Code/Code.tsx
@@ -1,45 +1,54 @@
-import React, { useLayoutEffect, useRef } from "react"
+import React, { useLayoutEffect, useMemo, useRef } from "react"
 
 import highlight from "highlight.js"
 import 'highlight.js/styles/github.css'
 import javascript from 'highlight.js/lib/languages/javascript'
+import typescript from 'highlight.js/lib/languages/typescript'
+import python from 'highlight.js/lib/languages/python'
+import html from 'highlight.js/lib/languages/html'
+import rust from 'highlight.js/lib/languages/rust'
 import php from 'highlight.js/lib/languages/php'
 import go from 'highlight.js/lib/languages/go'
 import scss from 'highlight.js/lib/languages/scss'
 import css from 'highlight.js/lib/languages/css'
 
 highlight.registerLanguage('javascript', javascript)
+highlight.registerLanguage('typescript', typescript)
+highlight.registerLanguage('python', python)
 highlight.registerLanguage('css', css)
 highlight.registerLanguage('scss', scss)
 highlight.registerLanguage('go', go)
 highlight.registerLanguage('php', php)
+highlight.registerLanguage('rust', rust)
+highlight.registerLanguage('html', html)
 
 export type CodeProps = {
     language: string,
-    mayNotWrap?: boolean,
+    mayWrap?: boolean,
     children: string | string[],
     code?: undefined,
 } | {
     language: string,
-    mayNotWrap?: boolean,
+    mayWrap?: boolean,
     code: string,
     children?: undefined,
 }
 
 export default (props: CodeProps) => {
-    const elemRef = useRef<any>()
-    let value = props.children ?? props.code
-    if (value instanceof Array && value !== null) {
-        value = value.join("\n")
-    }
+    const highlighted = useMemo(() => {
+        let value = props.children ?? props.code
+        if (value instanceof Array && value !== null) {
+            value = value.join("\n")
+        }
 
-    if (typeof value === "string") {
-        value = value.trim()
-    }
+        if (typeof value === "string") {
+            value = value.trim()
+        }
 
-    const highlighted = highlight.highlight(value as string, {
-        language: props.language,
-    }).value
+        return highlight.highlight(value as string, {
+            language: props.language,
+        }).value
+    }, [props.children, props.code])
 
-    return <pre ref={elemRef} className={`language-${props.language}`}><code dangerouslySetInnerHTML={{__html: highlighted}}></code></pre>
+    return <pre className={`language-${props.language}`}><code dangerouslySetInnerHTML={{ __html: highlighted }}></code></pre>
 }
