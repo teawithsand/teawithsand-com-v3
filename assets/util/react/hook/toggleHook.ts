64fb20c6a79dfc3daf193af02d6a69b17f0ac87b
teawithsand <teawithsand@gmail.com>
2022-04-12 23:30:48 +0000

    Implemented toggleHook, which is useful for dropdowns

---
+++ assets/util/react/hook/toggleHook.ts
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export const useToggle = (init?: boolean) => {
+    const [toggled, setToggle] = useState(!!init)
+    return [
+        toggled,
+        () => {
+            setToggle(!toggled)
+        },
+    ]
+}
+export const useNamedToggle = (init?: boolean) => {
+    const [toggled, setToggle] = useState(!!init)
+    return {
+        toggled: toggled,
+        toggle: () => {
+            setToggle(!toggled)
+        },
+    }
+}
