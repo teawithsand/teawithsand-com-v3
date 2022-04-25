253ce1d93453c1e7022456b0bd3597a41439a37c
teawithsand <teawithsand@gmail.com>
2022-04-25 07:45:16 +0000

    Added Point for mouse input

--- assets/Component/Paint/paint/ui/PaintUIInput.ts
+++ assets/Component/Paint/paint/ui/PaintUIInput.ts
@@ -1,9 +1,11 @@
+import { Point } from "../../primitive"
+
 export type PaintUIInput = {
     type: "mouse",
     x: number,
     y: number,
+    point: Point, // should be used in favour of x and y
     pressed: boolean,
-    pressedToggled: boolean,
 } | {
     type: "setting-change",
     data: SettingChangeUIInput,
