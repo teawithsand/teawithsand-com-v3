4643a5077d2deaa43c686ed99a54900531d9d6fc
teawithsand <teawithsand@gmail.com>
2022-04-26 00:09:01 +0000

    Changed layer definition to use array instead of iterable

--- assets/Component/DOMPaint/layer/Layer.ts
+++ assets/Component/DOMPaint/layer/Layer.ts
@@ -4,7 +4,7 @@ import PaintLayerMetadata from "./LayerMetadata";
 
 export default class PaintLayer {
     constructor(
-        public elements: Iterable<PaintElement>,
+        public elements: PaintElement[],
         public metadata: PaintLayerMetadata,
         public processor?: PaintElementProcessor,
     ) { }
