104b41dbdae509615df4ff40d8f33333e45a908e
teawithsand <teawithsand@gmail.com>
2022-04-26 18:25:52 +0000

    Added new implementation of paintelements, which exists along with old one

---
+++ assets/Component/DOMPaint/newelement/PaintElementData.ts
@@ -0,0 +1,11 @@
+/**
+ * Interface, which is implemented by data of all PaintElements.
+ */
+export default interface PaintElementData {
+    // CSS filters to be applied to given element.
+    filters: PaintElementFilter[],
+}
+
+export type PaintElementFilter = {
+    // NIY definition
+}
