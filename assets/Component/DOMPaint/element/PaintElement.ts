31b510db205f5883b38168ffb9ba02757779e8f3
teawithsand <teawithsand@gmail.com>
2022-04-26 01:44:35 +0000

    Added immutability todo

--- assets/Component/DOMPaint/element/PaintElement.ts
+++ assets/Component/DOMPaint/element/PaintElement.ts
@@ -1,5 +1,6 @@
 import PaintElementPostprocess from "./PaintElementPostprocess"
 
+// TODO(teawithsand): make instances of this class immutable
 export default abstract class PaintElement {
     abstract renderId: string
     abstract postprocess: PaintElementPostprocess
