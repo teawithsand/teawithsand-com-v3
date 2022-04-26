bda460f8e1a8e464c9ed72336ceeb80b1ce975fb
teawithsand <teawithsand@gmail.com>
2022-04-26 00:09:42 +0000

    Refactored paint tool stuff to use new EventSourcing based scene

--- assets/Component/DOMPaint/ui/tool/PaintToolCallbacks.ts
+++ assets/Component/DOMPaint/ui/tool/PaintToolCallbacks.ts
@@ -1,10 +1,8 @@
 import React from "react"
-import PaintScene from "../../scene/PaintScene"
 import PaintTool from "./PaintTool"
 
 type PaintToolCallbacks = {
     updateTopLevelStyles(styles: React.CSSProperties): void
-    updateScene(scene: PaintScene): void
     updateTool(tool: PaintTool): void
     disableSelf(): void
 }
--- assets/Component/DOMPaint/ui/tool/PaintToolEnvironment.ts
+++ assets/Component/DOMPaint/ui/tool/PaintToolEnvironment.ts
@@ -1,8 +1,12 @@
+import { EventSourcing } from "@app/util/lang/eventSourcing"
+import PaintScene from "../../scene/PaintScene"
+import PaintSceneMutation from "../../scene/PaintSceneMutation"
 import UIState from "../UIState"
 
 type PaintToolEnvironment = {
-    parentElementRef: { readonly current: HTMLElement },
-    uiState: UIState,
+    readonly parentElementRef: { readonly current: HTMLElement },
+    readonly uiState: Readonly<UIState>,
+    readonly scene: EventSourcing<PaintScene, PaintSceneMutation>,
 }
 
 export default PaintToolEnvironment
