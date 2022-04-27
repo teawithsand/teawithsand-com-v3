560451fd4a28c51124683b9f6c96f6258893547a
teawithsand <teawithsand@gmail.com>
2022-04-27 10:15:45 +0000

    Fixd linked in link

--- assets/Component/endpoints.ts
+++ assets/Component/endpoints.ts
@@ -7,8 +7,8 @@ export const paintPath = "/tool/paint"
 export const homePath = "/"
 
 export const linkGithub = "https://github.com/teawithsand"
-export const linkLinkedIn = "https://github.com/teawithsand"
-export const linkYouTube = "https://github.com/teawithsand"
+export const linkLinkedIn = "https://www.linkedin.com/in/przemyslaw-glowacki-d2f37f2e/"
+export const linkYouTube = "https://github.com/teawithsand" // NIY for now
 
 export const email = "contact@przemyslawglowacki.com"
 export const linkEmail = `mailto:contact@${email}` 
