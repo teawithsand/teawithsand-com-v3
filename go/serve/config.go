a709944aca0d8fd8e1bd55593ca45dea461e0d70
teawithsand <teawithsand@gmail.com>
2022-04-28 09:52:54 +0000

    Fixed config and added untested https server support

--- go/serve/config.go
+++ go/serve/config.go
@@ -1,7 +1,11 @@
 package serve
 
 type Config struct {
-	Env           string `mapstructure:"TWSBLOG_ENV"`
-	ListenAddress string `mapstructure:"TWSBLOG_LISTEN_ADDRESS"`
-	DebugPath     string `mapstructure:"TWSBLOG_DEBUG_PATH"`
+	Env               string `mapstructure:"TWSBLOG_ENV"`
+	HTTPListenAddress string `mapstructure:"TWSBLOG_HTTP_LISTEN_ADDRESS"`
+	DebugPath         string `mapstructure:"TWSBLOG_DEBUG_PATH"`
+
+	HTTPSListenAddress  string `mapstructure:"TWSBLOG_HTTPS_LISTEN_ADDRESS"`
+	HTTPSCertPath       string `mapstructure:"TWSBLOG_HTTPS_CERT_PATH"`
+	HTTPSPrivateKeyPath string `mapstructure:"TWSBLOG_HTTPS_KEY_PATH"`
 }
--- go/serve/serve.go
+++ go/serve/serve.go
@@ -1,31 +1,107 @@
 package serve
 
 import (
+	"crypto/tls"
+	"fmt"
 	"net"
+	"net/http"
+	"sync"
 
 	"github.com/teawithsand/twsblog/util/cfg"
 )
 
 func Run() (err error) {
 	var config Config
+
 	err = cfg.ReadConfig(&config)
 	if err != nil {
 		return
 	}
+
+	if len(config.HTTPListenAddress) == 0 && len(config.HTTPSListenAddress) == 0 {
+		err = fmt.Errorf("no address to listen on")
+		return
+	}
+
 	r, err := MakeRouter(config)
 	if err != nil {
 		return
 	}
 
+	wg := &sync.WaitGroup{}
+
 	s, err := MakeServer(r)
 	if err != nil {
 		return
 	}
-	l, err := net.Listen("tcp", config.ListenAddress)
-	if err != nil {
-		return
+
+	if len(config.HTTPListenAddress) > 0 {
+
+		var l net.Listener
+		l, err = net.Listen("tcp", config.HTTPListenAddress)
+		if err != nil {
+			return
+		}
+
+		if len(config.HTTPSListenAddress) > 0 {
+			redirectToHTTPS := func(w http.ResponseWriter, req *http.Request) {
+				http.Redirect(w, req, "https://"+req.Host+req.RequestURI, http.StatusMovedPermanently)
+			}
+			srv := &http.Server{
+				Handler:           http.HandlerFunc(redirectToHTTPS),
+				ReadTimeout:       s.ReadTimeout,
+				WriteTimeout:      s.WriteTimeout,
+				IdleTimeout:       s.IdleTimeout,
+				MaxHeaderBytes:    s.MaxHeaderBytes,
+				ReadHeaderTimeout: s.ReadHeaderTimeout,
+				BaseContext:       s.BaseContext,
+			}
+
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				err := srv.Serve(l)
+				_ = err
+			}()
+		} else {
+			s := *s // copy server
+
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				err := s.Serve(l)
+				_ = err
+			}()
+		}
 	}
 
-	err = s.Serve(l)
+	if len(config.HTTPSListenAddress) > 0 {
+		returnCert := func(helloInfo *tls.ClientHelloInfo) (*tls.Certificate, error) {
+			cer, err := tls.LoadX509KeyPair(config.HTTPSCertPath, config.HTTPSPrivateKeyPath)
+			if err != nil {
+				return nil, err
+			}
+
+			return &cer, nil
+		}
+
+		var l net.Listener
+		l, err = tls.Listen("tcp", config.HTTPSListenAddress, &tls.Config{
+			GetCertificate: returnCert,
+			MinVersion:     tls.VersionTLS13,
+		})
+		if err != nil {
+			return
+		}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			err := s.Serve(l)
+			_ = err
+		}()
+	}
+
+	wg.Wait()
 	return
 }
