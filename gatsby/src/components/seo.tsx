f2e629c9b9d2662a29c576c1f539ce127bf41542
teawithsand <teawithsand@gmail.com>
2022-04-30 14:15:22 +0000

    Fixed bio and seo components and cleaned up blog post

--- gatsby/src/components/seo.tsx
+++ gatsby/src/components/seo.tsx
@@ -1,16 +1,21 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
-import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-const Seo = ({ description, lang, meta, title }) => {
+type MetaProps = {
+    content: string,
+    name: string
+} | {
+    property: string,
+    content: string,
+}
+
+const Seo = (props: {
+	description?: string
+	lang?: string
+	meta?: MetaProps[]
+	title?: string
+}) => {
 	const { site } = useStaticQuery(
 		graphql`
 			query {
@@ -18,25 +23,23 @@ const Seo = ({ description, lang, meta, title }) => {
 					siteMetadata {
 						title
 						description
-						social {
-							twitter
-						}
 					}
 				}
 			}
 		`
 	)
 
+	const { description, lang, meta, title } = props
+
 	const metaDescription = description || site.siteMetadata.description
-	const defaultTitle = site.siteMetadata?.title
+	const defaultTitle = site.siteMetadata?.title ?? ""
 
 	return (
 		<Helmet
 			htmlAttributes={{
 				lang,
 			}}
-			title={title}
-			titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+			title={title || defaultTitle}
 			meta={[
 				{
 					name: `description`,
@@ -54,7 +57,8 @@ const Seo = ({ description, lang, meta, title }) => {
 					property: `og:type`,
 					content: `website`,
 				},
-				{
+				/*
+                {
 					name: `twitter:card`,
 					content: `summary`,
 				},
@@ -70,22 +74,10 @@ const Seo = ({ description, lang, meta, title }) => {
 					name: `twitter:description`,
 					content: metaDescription,
 				},
-			].concat(meta)}
+                */
+			].concat(meta ?? [])}
 		/>
 	)
-}
-
-Seo.defaultProps = {
-	lang: `en`,
-	meta: [],
-	description: ``,
-}
-
-Seo.propTypes = {
-	description: PropTypes.string,
-	lang: PropTypes.string,
-	meta: PropTypes.arrayOf(PropTypes.object),
-	title: PropTypes.string.isRequired,
 }
 
 export default Seo
--- gatsby/src/templates/blog-post.js
+++ gatsby/src/templates/blog-post.js
@@ -1,14 +1,13 @@
 import * as React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
-import Bio from "../components/bio"
 import Seo from "../components/seo"
 import Layout from "@app/components/layout/Layout"
 
 const BlogPostTemplate = ({ data, location }) => {
 	const post = data.markdownRemark
-	const siteTitle = data.site.siteMetadata?.title || `Title`
-	const { previous, next } = data
+	// const siteTitle = data.site.siteMetadata?.title || `Title`
+	// const { previous, next } = data
 
 	return (
 		<Layout>
@@ -16,50 +15,7 @@ const BlogPostTemplate = ({ data, location }) => {
 				title={post.frontmatter.title}
 				description={post.frontmatter.description || post.excerpt}
 			/>
-			<article
-				className="blog-post"
-				itemScope
-				itemType="http://schema.org/Article"
-			>
-				<header>
-					<h1 itemProp="headline">{post.frontmatter.title}</h1>
-					<p>{post.frontmatter.date}</p>
-				</header>
-				<section
-					dangerouslySetInnerHTML={{ __html: post.html }}
-					itemProp="articleBody"
-				/>
-				<hr />
-				<footer>
-					<Bio />
-				</footer>
-			</article>
-			<nav className="blog-post-nav">
-				<ul
-					style={{
-						display: `flex`,
-						flexWrap: `wrap`,
-						justifyContent: `space-between`,
-						listStyle: `none`,
-						padding: 0,
-					}}
-				>
-					<li>
-						{previous && (
-							<Link to={previous.fields.slug} rel="prev">
-								← {previous.frontmatter.title}
-							</Link>
-						)}
-					</li>
-					<li>
-						{next && (
-							<Link to={next.fields.slug} rel="next">
-								{next.frontmatter.title} →
-							</Link>
-						)}
-					</li>
-				</ul>
-			</nav>
+			
 		</Layout>
 	)
 }
