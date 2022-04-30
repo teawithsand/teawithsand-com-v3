75f8727c3979374934b524acac07d9c7af6263db
teawithsand <teawithsand@gmail.com>
2022-04-30 11:03:27 +0000

    Created basic webite with tea animation on it

---
+++ gatsby/src/pages/index.tsx
@@ -0,0 +1,36 @@
+import * as React from "react"
+import { Link, graphql } from "gatsby"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import TeaAnimation from "@app/components/tea-animation/TeaAnimation"
+
+const BlogIndex = (props: any) => {
+	return <TeaAnimation />
+}
+
+export default BlogIndex
+
+export const pageQuery = graphql`
+	query {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+		allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+			nodes {
+				excerpt
+				fields {
+					slug
+				}
+				frontmatter {
+					date(formatString: "MMMM DD, YYYY")
+					title
+					description
+				}
+			}
+		}
+	}
+`
