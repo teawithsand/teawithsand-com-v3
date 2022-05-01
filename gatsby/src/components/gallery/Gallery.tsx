fa569e96af39360485b455b96a8eede79cd58615
teawithsand <teawithsand@gmail.com>
2022-05-01 01:03:05 +0000

    Implemented working gallery

--- gatsby/src/components/gallery/Gallery.tsx
+++ gatsby/src/components/gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 
 import * as styles from "./gallery.module.scss"
 import { DissolveGalleryDisplay } from "@app/components/gallery/GalleryDisplay"
@@ -7,17 +7,32 @@ import GalleryItemDisplay from "@app/components/gallery/GalleryItemDisplay"
 import classnames from "@app/util/lang/classnames"
 import { useFullscreen } from "@app/util/react/hook/useFullscreen"
 
-export type GalleryMode = "normal" | "fullscreen" | "zoom"
+import { useGesture } from "@use-gesture/react"
 
 export type GalleryProps = {
 	provider: GalleryItemProvider
+	autoSwitchInitializer?: () => number
 }
 
-const Gallery = (props: GalleryProps) => {
-	const { provider } = props
+const defaultAutoSwitchDelay = 30 * 1000
 
-	const [mode, setMode] = useState<GalleryMode>("normal")
+const Gallery = (props: GalleryProps) => {
+	const { provider, autoSwitchInitializer } = props
+
 	const [itemIndex, setItemIndex] = useState<number>(0)
+	const [showBottomBar, setShowBottomBar] = useState<boolean>(true)
+
+	const [autoSwitchDelay, setAutoSwitchDelay] = useState(
+		autoSwitchInitializer ?? defaultAutoSwitchDelay
+	)
+
+	const fsc = useFullscreen({})
+
+	const showControls = provider.itemCount > 1
+	const hideIfNotShowControlsStyle = showControls ? {} : { display: "none" }
+
+	// TODO(teawithsand): remove it
+	const mode = "normal"
 
 	const currentItem = useMemo(
 		() =>
@@ -51,11 +66,6 @@ const Gallery = (props: GalleryProps) => {
 		return items
 	}, [provider, mode])
 
-	const fsc = useFullscreen({})
-
-	const showControls = provider.itemCount > 1
-	const hideIfNotShowControlsStyle = showControls ? {} : { display: "none" }
-
 	const onBottomBarTap = (i: number) => {
 		while (i < 0) {
 			i += provider.itemCount
@@ -71,13 +81,124 @@ const Gallery = (props: GalleryProps) => {
 		onBottomBarTap(itemIndex - 1)
 	}
 
+	/*
+	useEffect(() => {
+		if (autoSwitchDelay > 0) {
+			const interval = setInterval(() => {
+				onRightSideTap()
+			}, autoSwitchDelay)
+
+			return () => {
+				clearInterval(interval)
+			}
+		}
+	}, [autoSwitchDelay])
+	*/
+
+	const bind = useGesture(
+		{
+			onDrag: ({
+				xy: [x, y],
+				initial: [sx, sy],
+				swipe: [swx, swy],
+				intentional,
+				first,
+				last,
+				memo,
+				tap,
+			}) => {
+				if (intentional) {
+					if (tap) {
+						if (!fsc.isFullscreen) {
+							fsc.enter()
+						}
+						setShowBottomBar(false)
+					} else if (swy === 0 && swx !== 0) {
+						if (swx < 0) onRightSideTap()
+						else onLeftSideTap()
+					} else {
+						if (swx === 0 && swy !== 0) {
+							if (swy < 0) {
+								setShowBottomBar(true)
+							} else {
+								setShowBottomBar(false)
+							}
+						}
+					}
+				}
+			},
+
+			// TODO(teawithsand): entering zoom/full mode on pinch
+			onPinch: () => {
+				return
+			},
+		},
+		{
+			drag: {
+				filterTaps: false,
+				preventDefault: true,
+			},
+			pinch: {},
+		}
+	) as unknown as () => unknown
+
 	return (
 		<div
 			className={classnames(
 				styles.gallery,
-				mode === "fullscreen" ? styles.galleryFullscreen : null
+				fsc.isFullscreen ? styles.galleryFullscreen : null,
+				!showBottomBar ? styles.galleryBottomBarHidden : null
 			)}
 		>
+			<div className={styles.topBar}>
+				<div className={styles.topBarCounter}>
+					{itemIndex + 1}/{provider.itemCount}
+				</div>
+				<div
+					className={classnames(
+						styles.topBarAutoSwitchProgress,
+						autoSwitchDelay > 0
+							? styles.topBarAutoSwitchProgressEnabled
+							: styles.topBarAutoSwitchProgressDisabled
+					)}
+					onAnimationIteration={() => {
+						onRightSideTap()
+					}}
+					style={{
+						["--progress-animation-duration" as any]: `${autoSwitchDelay}ms`,
+					}}
+				></div>
+				<div className={styles.topBarOptions}>
+					<button
+						className={styles.topBarOptionsBtn}
+						onClick={() => {
+							fsc.setFullscreen(!fsc.isFullscreen)
+						}}
+					>
+						{!fsc.isFullscreen ? "FULLSCREEN" : "EXIT FULLSCREEN"}
+					</button>
+					<button
+						className={styles.topBarOptionsBtn}
+						onClick={() => {
+							setShowBottomBar(!showBottomBar)
+						}}
+					>
+						{showBottomBar ? "HIDE BOTTOM BAR" : "SHOW BOTTOM BAR"}
+					</button>
+					<button
+						className={styles.topBarOptionsBtn}
+						onClick={() => {
+							if (autoSwitchDelay > 0) {
+								setAutoSwitchDelay(-1)
+							} else {
+								setAutoSwitchDelay(defaultAutoSwitchDelay)
+							}
+						}}
+					>
+						{autoSwitchDelay > 0 ? "DISABLE AUTO SWITCH" : "ENABLE AUTO SWITCH"}
+					</button>
+				</div>
+			</div>
 			<div className={styles.mainBar}>
 				<div
 					className={styles.mainBarLeftOverlay}
@@ -85,7 +206,7 @@ const Gallery = (props: GalleryProps) => {
 					onClick={() => onLeftSideTap()}
 				></div>
 
-				<div className={styles.mainBarDisplayedEntryWrapper}>
+				<div className={styles.mainBarDisplayedEntryWrapper} {...bind()}>
 					<DissolveGalleryDisplay item={currentItem} />
 				</div>
 
@@ -110,7 +231,8 @@ const Gallery = (props: GalleryProps) => {
 			<div
 				className={classnames(
 					styles.bottomBar,
-					showControls ? styles.bottomBarClickable : null
+					showControls ? styles.bottomBarClickable : null,
+					!showBottomBar ? styles.bottomBarHidden : null
 				)}
 			>
 				{thumbnails.map((img, i) => (
@@ -125,7 +247,10 @@ const Gallery = (props: GalleryProps) => {
 					// I've spent too much time debugging it, so this div stays here.
 					<div
 						key={img.key}
-						className={styles.bottomBarEntryWrapper}
+						className={classnames(
+							styles.bottomBarEntryWrapper,
+							i === itemIndex ? styles.bottomBarEntryWrapperActive : null
+						)}
 						onClick={() => onBottomBarTap(i)}
 					>
 						<GalleryItemDisplay item={img} />
