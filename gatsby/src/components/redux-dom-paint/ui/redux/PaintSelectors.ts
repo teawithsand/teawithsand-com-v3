71cfc42d864a5a32980fb242eb6b3af6132d6fc8
teawithsand <teawithsand@gmail.com>
2022-05-02 23:20:16 +0000

    Implemented new selectors for redux store

--- gatsby/src/components/redux-dom-paint/ui/redux/PaintSelectors.ts
+++ gatsby/src/components/redux-dom-paint/ui/redux/PaintSelectors.ts
@@ -1,7 +1,31 @@
+import {
+	PathFillData,
+	PathStrokeData,
+} from "@app/components/dom-paint/element/impls/PathPaintElement"
 import PaintState from "@app/components/redux-dom-paint/ui/redux/PaintState"
 import { useSelector } from "react-redux"
 
 /**
  * Typed wrapper over useSelector from react-redux.
  */
-export const usePaintStateSelector = <T>(selector: (ps: PaintState) => T) => useSelector<PaintState, T>(selector)
+export const usePaintStateSelector = <T>(selector: (ps: PaintState) => T) =>
+	useSelector<PaintState, T>(selector)
+
+export const usePathStrokeData = (): PathStrokeData =>
+	usePaintStateSelector(s => ({
+		color: s.uiState.drawColor,
+		linecap: s.uiState.pathToolState.lineCapStyle,
+		linejoin: s.uiState.pathToolState.lineJoinStyle,
+		size: s.uiState.pathToolState.strokeSize,
+	}))
+
+export const usePathFillData = (): PathFillData | null =>
+	usePaintStateSelector(s =>
+		s.uiState.fillColor
+			? {
+					color: s.uiState.fillColor,
+			  }
+			: null
+	)
+
+// TODO(teawithsand): here add selector for stroke
