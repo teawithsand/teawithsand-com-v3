1b6785d40a5828df432c92265b4806c0aa61bdf5
teawithsand <teawithsand@gmail.com>
2022-05-02 11:47:09 +0000

    Implemented working PrimPaintSceneMutation along with inversing and tests

--- gatsby/src/components/dom-paint/nui/redux/PrimPaintSceneMutation.spec.ts
+++ gatsby/src/components/dom-paint/nui/redux/PrimPaintSceneMutation.spec.ts
@@ -7,10 +7,11 @@ import {
 } from "@app/components/dom-paint/nui/redux/PrimPaintScene"
 import PrimPaintSceneMutation, {
 	applyMutationOnDraft,
+	inverseMutation,
 } from "@app/components/dom-paint/nui/redux/PrimPaintSceneMutation"
 import produce from "immer"
 
-const makeTest =
+const makeMutationTest =
 	(data: {
 		initialScene?: PrimPaintScene | undefined
 		targetScene: PrimPaintScene
@@ -18,9 +19,9 @@ const makeTest =
 		disableNames?: boolean
 	}) =>
 	() =>
-		doTest(data)
+		doMutationTest(data)
 
-const doTest = (data: {
+const doMutationTest = (data: {
 	initialScene?: PrimPaintScene | undefined
 	targetScene: PrimPaintScene
 	mutations: PrimPaintSceneMutation[]
@@ -51,7 +52,74 @@ const doTest = (data: {
 		}
 	})
 
-	expect(initialScene).toEqual(targetScene)
+	expect(targetScene).toEqual(initialScene)
+}
+
+const makeInverseTest =
+	(data: {
+		initialScene?: PrimPaintScene | undefined
+		mutations: PrimPaintSceneMutation[]
+	}) =>
+	() =>
+		doInverseTest(data)
+
+const doInverseTest = (data: {
+	initialScene?: PrimPaintScene | undefined
+	mutations: PrimPaintSceneMutation[]
+}) => {
+	let { initialScene, mutations } = data
+
+	const scenes: PrimPaintScene[] = []
+
+	initialScene = initialScene ?? initialPrimPaintScene
+
+	let i = 0
+	for (const m of mutations) {
+		try {
+			let scene
+			if (i === 0) {
+				scene = initialScene
+			} else {
+				scene = scenes[i - 1]
+			}
+			const newScene = produce(scene, draft => applyMutationOnDraft(draft, m))
+			scenes.push(newScene)
+		} finally {
+			i++
+		}
+	}
+
+	for (let i = scenes.length - 1; i >= 0; i--) {
+		const mutatedScene = scenes[i]
+		const m = mutations[i]
+
+		let originalScene
+
+		if (i === 0) {
+			originalScene = initialScene
+		} else {
+			originalScene = scenes[i - 1]
+		}
+
+		const inv = inverseMutation(originalScene, m)
+		let invertedScene = produce(mutatedScene, draft =>
+			applyMutationOnDraft(draft, inv)
+		)
+
+		invertedScene = produce(invertedScene, draft => {
+			for (const l of draft.layers) {
+				l.id = "" // unset ids for simplicity of comparison
+			}
+		})
+
+		originalScene = produce(originalScene, draft => {
+			for (const l of draft.layers) {
+				l.id = "" // unset ids for simplicity of comparison
+			}
+		})
+
+		expect(originalScene).toEqual(invertedScene)
+	}
 }
 
 // note: elements really only have to be different
@@ -109,11 +177,29 @@ const scene = (layers: PrimPaintLayer[]) => ({
 	layers,
 })
 
+const srcScene4 = scene([
+	layer(layerData("l0", [element(0)])),
+	layer(layerData("l1", [element(1)])),
+	layer(layerData("l2", [element(2)])),
+	layer(layerData("l3", [element(3)])),
+])
+
 describe("PrimPaintScene", () => {
 	describe("mutations", () => {
-		it(
-			"can do push layer",
-			makeTest({
+		it("can do push layer", () => {
+			doMutationTest({
+				initialScene: scene([]),
+				targetScene: scene([
+					layer(layerData("added", [element(2), element(3)])),
+				]),
+				mutations: [
+					{
+						type: "push-layer",
+						data: layerData("added", [element(2), element(3)]),
+					},
+				],
+			})
+			doMutationTest({
 				initialScene: scene([
 					layer(layerData("l0", [element(0)])),
 					layer(layerData("l1", [element(1)])),
@@ -130,11 +216,8 @@ describe("PrimPaintScene", () => {
 					},
 				],
 			})
-		)
 
-		it(
-			"can handle layer insertion",
-			makeTest({
+			doMutationTest({
 				initialScene: scene([
 					layer(layerData("l0", [element(0)])),
 					layer(layerData("l1", [element(1)])),
@@ -148,20 +231,51 @@ describe("PrimPaintScene", () => {
 					{
 						type: "push-layer",
 						data: layerData("added", [element(2), element(3)]),
-						beforeIndex: 1,
+						beforeLayerIndex: 1,
 					},
 				],
 			})
-		)
+		})
 
-		it("can handle later moving", () => {
+		it("can do drop layer", () => {
+			const srcScene = srcScene4
+			doMutationTest({
+				initialScene: srcScene,
+				targetScene: scene([
+					layer(layerData("l0", [element(0)])),
+					layer(layerData("l1", [element(1)])),
+					layer(layerData("l2", [element(2)])),
+				]),
+				mutations: [
+					{
+						type: "drop-layer",
+					},
+				],
+			})
+
+			for (let i = 0; i < srcScene.layers.length; i++) {
+				doMutationTest({
+					initialScene: srcScene,
+					targetScene: scene(srcScene.layers.filter((v, j) => j !== i)),
+					mutations: [
+						{
+							type: "drop-layer",
+							layerIndex: i,
+						},
+					],
+				})
+			}
+		})
+
+		it("can do move", () => {
 			const srcScene = scene([
 				layer(layerData("l0", [element(0)])),
 				layer(layerData("l1", [element(1)])),
 				layer(layerData("l2", [element(2)])),
 				layer(layerData("l3", [element(3)])),
 			])
-			doTest({
+
+			doMutationTest({
 				initialScene: srcScene,
 				targetScene: scene([
 					layer(layerData("l1", [element(1)])),
@@ -178,7 +292,7 @@ describe("PrimPaintScene", () => {
 			})
 
 			for (let i = 0; i < srcScene.layers.length; i++) {
-				doTest({
+				doMutationTest({
 					initialScene: srcScene,
 					targetScene: srcScene,
 					mutations: [
@@ -191,7 +305,7 @@ describe("PrimPaintScene", () => {
 				})
 			}
 
-			doTest({
+			doMutationTest({
 				initialScene: srcScene,
 				targetScene: scene([
 					layer(layerData("l1", [element(1)])),
@@ -208,7 +322,23 @@ describe("PrimPaintScene", () => {
 				],
 			})
 
-			doTest({
+			doMutationTest({
+				initialScene: srcScene,
+				targetScene: scene([
+					layer(layerData("l1", [element(1)])),
+					layer(layerData("l2", [element(2)])),
+					layer(layerData("l3", [element(3)])),
+					layer(layerData("l0", [element(0)])),
+				]),
+				mutations: [
+					{
+						type: "move-layer",
+						index: 0,
+					},
+				],
+			})
+
+			doMutationTest({
 				initialScene: srcScene,
 				targetScene: scene([
 					layer(layerData("l2", [element(2)])),
@@ -224,8 +354,304 @@ describe("PrimPaintScene", () => {
 					},
 				],
 			})
+
+			doMutationTest({
+				initialScene: srcScene,
+				targetScene: scene([
+					layer(layerData("l1", [element(1)])),
+					layer(layerData("l2", [element(2)])),
+					layer(layerData("l3", [element(3)])),
+					layer(layerData("l0", [element(0)])),
+				]),
+				mutations: [
+					{
+						type: "move-layer",
+						index: 0,
+						beforeIndex: 4,
+					},
+				],
+			})
 		})
-        
-        // TODO(teawithsand): more tests here for coverage
+
+		it("can do drop layer elements", () => {
+			let i = 0
+			const initialScene = scene([
+				layer(
+					layerData("l1", [
+						element(i++),
+						element(i++),
+						element(i++),
+						element(i++),
+						element(i++),
+						element(i++),
+					])
+				),
+			])
+
+			doMutationTest({
+				initialScene,
+				targetScene: scene([layer(layerData("l1", []))]),
+				mutations: [
+					{
+						type: "drop-layer-elements",
+						layerIndex: 0,
+						toElementIndex: initialScene.layers[0].elements.length,
+					},
+				],
+			})
+			doMutationTest({
+				initialScene,
+				targetScene: scene([layer(layerData("l1", []))]),
+				mutations: [
+					{
+						type: "drop-layer-elements",
+						layerIndex: 0,
+						fromElementIndex: 0,
+					},
+				],
+			})
+			doMutationTest({
+				initialScene,
+				targetScene: scene([layer(layerData("l1", []))]),
+				mutations: [
+					{
+						type: "drop-layer-elements",
+						layerIndex: 0,
+					},
+				],
+			})
+			doMutationTest({
+				initialScene,
+				targetScene: scene([layer(layerData("l1", []))]),
+				mutations: [
+					{
+						type: "drop-layer-elements",
+						layerIndex: 0,
+						fromElementIndex: 0,
+						toElementIndex: initialScene.layers[0].elements.length,
+					},
+				],
+			})
+		})
+
+		// TODO(teawithsand): more tests here for coverage
+	})
+
+	describe("inverse mutations", () => {
+		it("can inverse push layer", () => {
+			const srcScene = srcScene4
+			doInverseTest({
+				initialScene: srcScene,
+				mutations: [
+					{
+						type: "push-layer",
+						data: layerData("pushed", [element(20)]),
+					},
+				],
+			})
+
+			for (let i = 0; i < srcScene.layers.length; i++) {
+				doInverseTest({
+					initialScene: srcScene,
+					mutations: [
+						{
+							type: "push-layer",
+							data: layerData("pushed", [element(20)]),
+							beforeLayerIndex: i,
+						},
+					],
+				})
+			}
+		})
+
+		it("can inverse drop layer", () => {
+			const srcScene = srcScene4
+
+			doInverseTest({
+				initialScene: srcScene,
+				mutations: [
+					{
+						type: "drop-layer",
+					},
+				],
+			})
+
+			for (let i = 0; i < srcScene.layers.length; i++) {
+				doInverseTest({
+					initialScene: srcScene,
+					mutations: [
+						{
+							type: "drop-layer",
+							layerIndex: i,
+						},
+					],
+				})
+			}
+		})
+
+		it("can inverse move layer", () => {
+			const srcScene = srcScene4
+
+			for (let i = 0; i < srcScene.layers.length; i++) {
+				doInverseTest({
+					initialScene: srcScene,
+					mutations: [
+						{
+							type: "move-layer",
+							index: i,
+						},
+					],
+				})
+			}
+
+			for (let i = 0; i < srcScene.layers.length; i++) {
+				for (let j = 0; j < srcScene.layers.length; j++) {
+					doInverseTest({
+						initialScene: srcScene,
+						mutations: [
+							{
+								type: "move-layer",
+								index: i,
+								beforeIndex: j,
+							},
+						],
+					})
+				}
+			}
+		})
+
+		it("can inverse push layer elements", () => {
+			const srcScene = srcScene4
+
+			doInverseTest({
+				initialScene: srcScene,
+				mutations: [
+					{
+						type: "push-layer-elements",
+						layerIndex: 0,
+						elements: [element(5), element(6)],
+					},
+				],
+			})
+
+			doInverseTest({
+				initialScene: srcScene,
+				mutations: [
+					{
+						type: "push-layer-elements",
+						layerIndex: 0,
+						elements: [],
+					},
+				],
+			})
+		})
+
+		it("can inverse drop layer elements", () => {
+			let i = 0
+			const srcScene = scene([
+				layer(
+					layerData("l0", [
+						element(i++),
+						element(i++),
+						element(i++),
+						element(i++),
+						element(i++),
+					])
+				),
+			])
+
+			const elements = [
+				...Array(srcScene.layers[0].elements.length).keys(),
+				undefined,
+			]
+			for (const i of elements) {
+				for (const j of elements) {
+					if (typeof i === "number" && typeof j === "number" && i > j) {
+						continue
+					}
+					doInverseTest({
+						initialScene: srcScene,
+						mutations: [
+							{
+								type: "drop-layer-elements",
+								layerIndex: 0,
+								fromElementIndex: i,
+								toElementIndex: j,
+							},
+						],
+					})
+				}
+			}
+		})
+
+		it("can inverse move layer element", () => {
+			let i = 0
+			const srcScene = scene([
+				layer(
+					layerData("l0", [
+						element(i++),
+						element(i++),
+						element(i++),
+						element(i++),
+						element(i++),
+					])
+				),
+				layer(
+					layerData("l0", [
+						element(i++),
+						element(i++),
+						element(i++),
+						element(i++),
+						element(i++),
+					])
+				),
+			])
+
+			const elements = [
+				...Array(srcScene.layers[0].elements.length).keys(),
+			]
+
+			// inter-layer move
+			for (const i of elements) {
+				for (const j of [...elements, undefined]) {
+					if (typeof i === "number" && typeof j === "number") {
+						continue
+					}
+					doInverseTest({
+						initialScene: srcScene,
+						mutations: [
+							{
+								type: "move-layer-element",
+								sourceLayerIndex: 0,
+								destinationLayerIndex: 1,
+								sourceElementIndex: i,
+								beforeDestinationElementIndex: j,
+							},
+						],
+					})
+				}
+			}
+
+			// same-layer move
+			for (const i of elements) {
+				for (const j of [...elements, undefined]) {
+					if (typeof i === "number" && typeof j === "number") {
+						continue
+					}
+					doInverseTest({
+						initialScene: srcScene,
+						mutations: [
+							{
+								type: "move-layer-element",
+								sourceLayerIndex: 0,
+								destinationLayerIndex: 0,
+								sourceElementIndex: i,
+								beforeDestinationElementIndex: j,
+							},
+						],
+					})
+				}
+			}
+		})
 	})
 })
--- gatsby/src/components/dom-paint/nui/redux/PrimPaintSceneMutation.ts
+++ gatsby/src/components/dom-paint/nui/redux/PrimPaintSceneMutation.ts
@@ -12,12 +12,12 @@ import { WritableDraft } from "immer/dist/internal"
 type PrimLayerMutation =
 	| {
 			type: "drop-layer"
-			index: number
+			layerIndex?: number | undefined // if undefined, drop last layer(AKA pop from stack)
 	  }
 	| {
 			type: "push-layer"
-			beforeIndex?: number | undefined
-			data: PrimPaintLayerData,
+			beforeLayerIndex?: number | undefined
+			data: PrimPaintLayerData
 	  }
 	| {
 			type: "move-layer"
@@ -26,7 +26,7 @@ type PrimLayerMutation =
 	  }
 	| {
 			type: "set-layer-metadata"
-			index: number
+			layerIndex: number
 			metadata: PrimPaintLayerMetadata
 	  }
 
@@ -38,11 +38,33 @@ type PrimElementMutation =
 			elements: PrimPaintElement[]
 	  }
 	| {
+			// This mutation is noop if fromElementIndex === toElementIndex
 			type: "drop-layer-elements"
+			layerIndex: number
+			fromElementIndex?: number | undefined // if undefined, points to 0 index; inclusive index
+			toElementIndex?: number | undefined // if undefined, points to last element's index; exclusive index
+	  }
+	| /*
+	{
+			// note: this mutation shouldn't be used
+			// with other app components
+			// it was created because
+			// it was impossible to express revert mutation of
+			// drop-layer-elements as push-layer-elements
+			type: "push-layer-elements-targets"
+			layerIndex: number
+			entries: {
+				beforeIndex: number
+				element: PrimPaintElement
+			}[]
+	  }
+	| {
+			type: "drop-layer-elements-targets"
 			layerIndex: number
 			elementIndices: number[]
 	  }
-	| {
+	| 
+	*/ {
 			type: "move-layer-element"
 			sourceLayerIndex: number
 			sourceElementIndex: number
@@ -50,51 +72,48 @@ type PrimElementMutation =
 			beforeDestinationElementIndex?: number | undefined
 	  }
 
-type PrimPaintSceneMutation = PrimLayerMutation | PrimElementMutation
+type PrimPaintSceneMutation =
+	| PrimLayerMutation
+	| PrimElementMutation
+	| {
+			type: "noop" // just noop
+	  }
 
 export default PrimPaintSceneMutation
+
+export class PaintSceneMutationApplyError extends Error {
+	constructor(message: string) {
+		super(message)
+		this.name = "PaintSceneMutationApplyError"
+	}
+}
 
 const ensureSceneHasLayer = (
 	s: WritableDraft<PrimPaintScene>,
 	i: number
 ): WritableDraft<PrimPaintLayer> => {
-	const id = generateUUID()
-	while (i >= s.layers.length) {
-		s.layers.push({
-			id: id,
-			elements: [],
-			metadata: {
-				isHidden: false,
-				name: `layer-${id}`,
-			},
-		})
-	}
+	// TODO(teawithsand): make sure that this function is not needed, since it's not compatible with reversing mutations
 
+	if (i < 0 || i >= s.layers.length)
+		throw new PaintSceneMutationApplyError(`Can't find layer with index ${i}`)
 	return s.layers[i]
 }
-
-export const revertMutationOnDraft = (
-	scene: WritableDraft<PrimPaintScene>,
-	mutation: PrimPaintSceneMutation
-) => {
-	throw new Error("NIY")
-}
-
-export const revertMutation = (
-	scene: PrimPaintScene,
-	mutation: PrimPaintSceneMutation
-) => produce(scene, draft => revertMutationOnDraft(draft, mutation))
 
 export const applyMutationOnDraft = (
 	scene: WritableDraft<PrimPaintScene>,
 	m: PrimPaintSceneMutation
 ) => {
 	if (m.type === "drop-layer") {
-		ensureSceneHasLayer(scene, m.index)
-		if (m.index === scene.layers.length - 1) {
+		if (m.layerIndex === undefined && scene.layers.length === 0) {
+			return // noop
+		}
+		const index = m.layerIndex ?? scene.layers.length - 1
+		ensureSceneHasLayer(scene, index)
+
+		if (m.layerIndex === scene.layers.length - 1) {
 			scene.layers.pop()
 		} else {
-			delete scene.layers[m.index]
+			scene.layers.splice(index, 1)
 		}
 	} else if (m.type === "push-layer") {
 		const layer = {
@@ -102,10 +121,10 @@ export const applyMutationOnDraft = (
 			elements: m.data.elements,
 			metadata: m.data.metadata,
 		}
-		scene.layers.splice(m.beforeIndex ?? scene.layers.length, 0, layer)
+		scene.layers.splice(m.beforeLayerIndex ?? scene.layers.length, 0, layer)
 	} else if (m.type === "set-layer-metadata") {
-		ensureSceneHasLayer(scene, m.index)
-		scene.layers[m.index].metadata = m.metadata
+		ensureSceneHasLayer(scene, m.layerIndex)
+		scene.layers[m.layerIndex].metadata = m.metadata
 	} else if (m.type === "move-layer") {
 		ensureSceneHasLayer(scene, m.index)
 		const beforeIndex = m.beforeIndex ?? scene.layers.length
@@ -120,17 +139,48 @@ export const applyMutationOnDraft = (
 		} else {
 			throw new Error("unreachable code")
 		}
-	} else if (m.type === "push-layer-elements") {
+	} /*
+	 else if (m.type === "push-layer-elements-targets") {
+		const l = ensureSceneHasLayer(scene, m.layerIndex)
+		const map = new Map<number, PrimPaintElement>()
+
+		for (const t of m.entries) {
+			map.set(t.beforeIndex, t.element)
+		}
+
+		l.elements = l.elements.flatMap((entry, i) => {
+			const element = map.get(i)
+			if (element) {
+				return [element, entry]
+			} else {
+				return [entry]
+			}
+		})
+	} 
+	*/ else if (m.type === "push-layer-elements") {
 		const l = ensureSceneHasLayer(scene, m.layerIndex)
 		l.elements.splice(
 			m.beforeElementIndex ?? l.elements.length,
 			0,
 			...m.elements
 		)
-	} else if (m.type === "drop-layer-elements") {
+	} /*
+	 else if (m.type === "drop-layer-elements-targets") {
 		const l = ensureSceneHasLayer(scene, m.layerIndex)
 		const idxSet = new Set(m.elementIndices)
 		l.elements = l.elements.filter((_, i) => !idxSet.has(i))
+	}*/ else if (m.type === "drop-layer-elements") {
+		const l = ensureSceneHasLayer(scene, m.layerIndex)
+		const fromIndex = m.fromElementIndex ?? 0
+		const toIndex = m.toElementIndex ?? l.elements.length
+		if (fromIndex > toIndex) {
+			throw new PaintSceneMutationApplyError(
+				`Can't drop from ${fromIndex} to ${toIndex}, from index > to index`
+			)
+		}
+		if (fromIndex !== toIndex) {
+			l.elements.splice(fromIndex, toIndex - fromIndex)
+		}
 	} else if (m.type === "move-layer-element") {
 		const sl = ensureSceneHasLayer(scene, m.sourceLayerIndex)
 		const dl = ensureSceneHasLayer(scene, m.destinationLayerIndex)
@@ -150,7 +200,7 @@ export const applyMutationOnDraft = (
 				throw new Error("unreachable code")
 			}
 		} else {
-			// no hard cases with index correction if do not wea are not targeting the same array
+			// no hard cases with index correction if do are not targeting the same array
 
 			const elementArray = sl.elements.splice(m.sourceElementIndex, 1)
 			dl.elements.splice(
@@ -159,6 +209,10 @@ export const applyMutationOnDraft = (
 				...elementArray
 			)
 		}
+	} else if (m.type === "noop") {
+		// noop
+	} else {
+		throw new Error(`Unknown mutation ${(m as unknown as any).type}`)
 	}
 }
 
@@ -166,3 +220,125 @@ export const applyMutation = (
 	scene: PrimPaintScene,
 	m: PrimPaintSceneMutation
 ) => produce(scene, draft => applyMutationOnDraft(draft, m))
+
+export const inverseMutation = (
+	scene: PrimPaintScene,
+	m: PrimPaintSceneMutation
+): PrimPaintSceneMutation => {
+	if (m.type === "push-layer") {
+		return {
+			type: "drop-layer",
+			layerIndex: m.beforeLayerIndex,
+		}
+	} else if (m.type === "drop-layer") {
+		const index = m.layerIndex ?? scene.layers.length - 1
+
+		return {
+			type: "push-layer",
+			data: scene.layers[index], // layer is valid layer data, although it can be shrunk for gc
+			beforeLayerIndex: index,
+		}
+	} else if (m.type === "move-layer") {
+		const index = m.index
+		const beforeIndex = m.beforeIndex ?? scene.layers.length
+
+		if (index === beforeIndex) {
+			return { type: "noop" }
+		} else if (index > beforeIndex) {
+			return {
+				type: "move-layer",
+				beforeIndex: index + 1,
+				index: beforeIndex,
+			}
+		} else {
+			return {
+				type: "move-layer",
+				beforeIndex: index,
+				index: beforeIndex - 1,
+			}
+		}
+	} else if (m.type === "push-layer-elements") {
+		/*
+		let elementIndices = []
+		const { beforeElementIndex } = m
+		if (beforeElementIndex === undefined) {
+			const offset =
+				scene.layers[m.layerIndex].elements.length - m.elements.length
+			elementIndices = [...Array(m.elements.length).keys()].map(v => v + offset)
+		} else {
+			elementIndices = [...Array(m.elements.length).keys()].map(
+				v => v + beforeElementIndex
+			)
+		}*/
+
+		const srcIndex =
+			m.beforeElementIndex ?? scene.layers[m.layerIndex].elements.length
+		const dstIndex = srcIndex + m.elements.length
+
+		return {
+			type: "drop-layer-elements",
+			layerIndex: m.layerIndex,
+			fromElementIndex: srcIndex,
+			toElementIndex: dstIndex,
+		}
+	} else if (m.type === "drop-layer-elements") {
+		const l = scene.layers[m.layerIndex]
+		return {
+			type: "push-layer-elements",
+			layerIndex: m.layerIndex,
+			elements: l.elements.slice(
+				m.fromElementIndex ?? 0,
+				m.toElementIndex ?? l.elements.length
+			),
+			beforeElementIndex: m.fromElementIndex ?? 0,
+		}
+	} else if (m.type === "move-layer-element") {
+		if (m.sourceLayerIndex === m.destinationLayerIndex) {
+			const index = m.sourceElementIndex
+			const beforeIndex = m.beforeDestinationElementIndex ?? scene.layers[m.sourceLayerIndex].elements.length
+	
+			if (index === beforeIndex) {
+				return { type: "noop" }
+			} else if (index > beforeIndex) {
+				return {
+					type: "move-layer-element",
+					destinationLayerIndex: m.sourceLayerIndex,
+					sourceLayerIndex: m.destinationLayerIndex,
+
+					beforeDestinationElementIndex: index + 1,
+					sourceElementIndex: beforeIndex,
+				}
+			} else {
+				return {
+					type: "move-layer-element",
+					destinationLayerIndex: m.sourceLayerIndex,
+					sourceLayerIndex: m.destinationLayerIndex,
+					beforeDestinationElementIndex: index,
+					sourceElementIndex: beforeIndex - 1,
+				}
+			}
+		} else {
+			return {
+				type: "move-layer-element",
+				destinationLayerIndex: m.sourceLayerIndex,
+				sourceLayerIndex: m.destinationLayerIndex,
+				sourceElementIndex:
+					m.beforeDestinationElementIndex ??
+					scene.layers[m.destinationLayerIndex].elements.length,
+				beforeDestinationElementIndex: m.sourceElementIndex,
+			}
+		}
+	} else if (m.type === "set-layer-metadata") {
+		return {
+			type: "set-layer-metadata",
+			layerIndex: m.layerIndex,
+			metadata: scene.layers[m.layerIndex].metadata,
+		}
+	} else if (m.type === "noop") {
+		return {
+			type: "noop",
+		}
+	} else {
+		throw new Error(`undefined mutation type ${(m as unknown as any).type}`)
+	}
+}
