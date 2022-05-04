5449c2c0cb0259fcae4355f6f7dd37d643011e41
teawithsand <teawithsand@gmail.com>
2022-05-04 06:43:56 +0000

    Ported contact page

---
+++ gatsby/src/components/pages/contact/Contact.tsx
@@ -0,0 +1,26 @@
+import { email, linkEmail, linkGithub, linkLinkedIn } from "@app/components/paths"
+import React from "react"
+
+import * as styles from "./contact.module.scss"
+
+export default () => {
+    return <main className={styles.pageContainer}>
+        <article>
+            <header className={styles.header}>
+                <h1>Contact</h1>
+            </header>
+
+            <div className={styles.pageBody}>
+                You can contact me via:
+                <ul>
+                    <li>(Preferably) Email <a href={linkEmail}>{email}</a></li>
+                </ul>
+                You can also find me at:
+                <ul>
+                    <li><a href={linkGithub}>GitHub</a></li>
+                    <li><a href={linkLinkedIn}>LinkedIn</a></li>
+                </ul>
+            </div>
+        </article>
+    </main>
+}
---
+++ gatsby/src/pages/contact.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "@app/components/layout/Layout"
+import Contact from "@app/components/pages/contact/Contact"
+
+export default () => {
+	return (
+		<Layout>
+			<Contact />
+		</Layout>
+	)
+}
