ba470d49aa0d323e74cbfb378c3727f458e30e53
teawithsand <teawithsand@gmail.com>
2022-05-04 11:37:57 +0000

    Ported home page

---
+++ gatsby/src/components/pages/home/home.tsx
@@ -0,0 +1,121 @@
+import React, { useRef } from "react"
+import { Link } from "gatsby"
+
+import * as styles from "./home.module.scss"
+import EmailImage from "./email.svg"
+
+import TeaAnimation from "@app/components/tea-animation/TeaAnimation"
+import { blogHomePath, contactPath, email, galleryPath, linkEmail, paintPath, portfolioPath } from "@app/components/paths"
+
+export default () => {
+	const firstSectionRef = useRef<HTMLElement | null>(null)
+
+	return (
+		<main className={styles["page-container"]}>
+			<article className={styles.header}>
+				<div className={styles.headerBackground}>
+					<TeaAnimation />
+				</div>
+				<div className={styles.headerOverlay}>
+					<h1>Teawithsand's website</h1>
+					<p>
+						Some notes, utils, blog and portfolio. Everything that
+						simple programmer needs in single webpack project.
+					</p>
+					<button
+						className={styles.headerScrollButton}
+						onClick={() => {
+							if (firstSectionRef.current) {
+								firstSectionRef.current.scrollIntoView({
+									behavior: "smooth",
+									block: "start",
+								})
+							}
+						}}
+					>
+						See more
+					</button>
+				</div>
+			</article>
+			<article className={styles.aboutWebsite} ref={firstSectionRef}>
+				<h2>About this website</h2>
+				<p>
+					It's my blog and website, where I can experiment with stuff,
+					without unnecessary development overhead. It also implements
+					some utils that I'd like to have.
+				</p>
+			</article>
+			<article className={styles.features}>
+				<div className={styles.featuresCardContainer}>
+					<div className={styles.featuresFeature}>
+						<h3>Blog</h3>
+						<p>
+							Blog consists of programming contents in {">"}90%.
+							You have been warned.
+						</p>
+						<Link
+							className={styles.featuresBtn}
+							to={blogHomePath}
+						>
+							See posts
+						</Link>
+					</div>
+					<div className={styles.featuresFeature}>
+						<h3>Portfolio</h3>
+						<p>
+							Summary of projects that I've created, along with
+							other useful links.
+						</p>
+						<Link
+							className={styles.featuresBtn}
+							to={portfolioPath}
+						>
+							See portfolio
+						</Link>
+					</div>
+                    <div className={styles.featuresFeature}>
+						<h3>Gallery</h3>
+						<p>
+							Simple, but extendable gallery for showing images
+						</p>
+						<Link className={styles.featuresBtn} to={galleryPath}>
+							Go to gallery
+						</Link>
+					</div>
+					<div className={styles.featuresFeature}>
+						<h3>Paint</h3>
+						<p>
+							Simple paint using vector, for painting simple
+							schematics
+						</p>
+						<Link className={styles.featuresBtn} to={paintPath}>
+							Go to paint
+						</Link>
+					</div>         
+				</div>
+			</article>
+			<article className={styles.contact}>
+				<div>
+					<header>
+						<h3>
+							<Link to={contactPath}>Contact</Link>
+						</h3>
+						<p>Via email</p>
+					</header>
+					<div className={styles.contactContainer}>
+						<div className={styles.contactEmail}>
+							<div>
+								<a href={linkEmail}>
+									<EmailImage width={100} height={100} />
+								</a>
+							</div>
+							<div>
+								<a href={linkEmail}>{email}</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</article>
+		</main>
+	)
+}
--- gatsby/src/pages/index.tsx
+++ gatsby/src/pages/index.tsx
@@ -1,43 +1,18 @@
-import React, { useMemo } from "react"
+import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "@app/components/layout/Layout"
-import { GalleryItem } from "@app/components/gallery"
-import Gallery from "@app/components/gallery/Gallery"
-import { ArrayGalleryItemProvider } from "@app/components/gallery/ItemProvider"
+import Home from "@app/components/pages/home/home"
 
-const BlogIndex = () => {
-	const items: GalleryItem[] = [
-		{
-			type: "image",
-			src: "https://upload.wikimedia.org/wikipedia/en/7/7d/Lenna_%28test_image%29.png",
-			alt: "Lena image",
-			key: "one",
-		},
-		{
-			type: "image",
-			src: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Wikipe-tan_in_Different_Anime_Styles.png/1024px-Wikipe-tan_in_Different_Anime_Styles.png",
-			alt: "Weeb stuff",
-			key: "two",
-		},
-		{
-			type: "image",
-			src: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jan_Matejko%2C_Bitwa_pod_Grunwaldem.jpg/2560px-Jan_Matejko%2C_Bitwa_pod_Grunwaldem.jpg",
-			alt: "bog",
-			key: "three",
-		},
-	]
-
-	const provider = useMemo(() => new ArrayGalleryItemProvider(items), [items])
-
+const Index = () => {
 	return (
 		<Layout>
-			<Gallery provider={provider} />
+			<Home />
 		</Layout>
 	)
 }
 
-export default BlogIndex
+export default Index
 
 export const pageQuery = graphql`
 	query {
@@ -48,96 +23,3 @@ export const pageQuery = graphql`
 		}
 	}
 `
-
-/*
-import * as React from "react"
-import { Link, graphql } from "gatsby"
-
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
-
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <Seo title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <Seo title="All posts" />
-      <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
-    </Layout>
-  )
-}
-
-export default BlogIndex
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      nodes {
-        excerpt
-        fields {
-          slug
-        }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
-        }
-      }
-    }
-  }
-`
-
-*/
