17961f11805f22c1f29872bcbc098c37da5b8f00
teawithsand <teawithsand@gmail.com>
2022-05-04 10:42:31 +0000

    Added support for global.scss

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -5,6 +5,7 @@ import "typeface-merriweather"
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
 
+import "./src/styles/global.scss"
 import "bootstrap/scss/bootstrap.scss"
 
 import "normalize.css/normalize.css"
