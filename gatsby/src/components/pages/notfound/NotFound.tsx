a427852cd1e73cc8236bd4de088a9b28f89ee276
teawithsand <teawithsand@gmail.com>
2022-05-04 13:58:39 +0000

    Fixed porting problems, so gatsby can process this page

--- gatsby/src/components/pages/notfound/NotFound.tsx
+++ gatsby/src/components/pages/notfound/NotFound.tsx
@@ -1,11 +1,11 @@
 import { homePath } from "@app/components/paths"
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link } from "gatsby"
 
 import * as styles from "./notFound.module.scss"
 
 export default () => {
-    return <div style={styles.parentContainer}>
+    return <div className={styles.parentContainer}>
         <h1>Page was not found</h1>
         <br />
         <Link className={styles.homeLink} to={homePath} >Go to home page</Link>
