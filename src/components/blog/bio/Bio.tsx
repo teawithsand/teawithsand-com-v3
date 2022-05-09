d250f4e4688d4dd0cc89f6435c00c39cefd51588
teawithsand <teawithsand@gmail.com>
2022-05-09 06:12:59 +0000

    Fixed how bio is displayed

--- src/components/blog/bio/Bio.tsx
+++ src/components/blog/bio/Bio.tsx
@@ -2,22 +2,48 @@ import * as styles from "./bio.module.scss"
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 
-export default (props: { orientation: "horizontal" | "vertical" }) => {
+const Bio = (props: { orientation: "horizontal" | "vertical" }) => {
 	const { orientation } = props
-	return (
-		<div className={styles.outerContainer}>
-			<StaticImage
-				src="./me.jpg"
-				alt="Me on the picture"
-				className={styles.image}
-			/>
-			<p className={styles.description}>
-				{/*
-				TODO(teawithsand): move this bio text somewhere else 
-				*/}
-				My name is Przemysław Głowacki(AKA teawithsand) and sometimes I
-				write some posts or create YouTube videos.
-			</p>
-		</div>
+	const description = (
+		<p className={styles.description}>
+			{/*
+			TODO(teawithsand): move this bio text somewhere else 
+			*/}
+			My name is <b>Przemysław Głowacki</b>(AKA <b>teawithsand</b>) and
+			sometimes I write some posts or create YouTube videos. I also write
+			software and sometimes it turns out to be useful.
+		</p>
 	)
+	if (orientation === "vertical") {
+		return (
+			<div className={styles.outer}>
+				<StaticImage
+					src="./me.png"
+					alt="Me on the picture"
+					className={styles.image}
+					width={100}
+					height={100}
+				/>
+				{description}
+			</div>
+		)
+	} else if (orientation === "horizontal") {
+		return (
+			<div className={styles.outer}>
+				<StaticImage
+					src="./me.png"
+					alt="Me on the picture"
+					className={styles.image}
+					width={100}
+					height={100}
+					style={{
+						float: "left",
+					}}
+				/>
+				{description}
+			</div>
+		)
+	}
 }
+
+export default Bio
