aa8579bd90dd7c2785e4df5ed9af0df5d8b56865
teawithsand <teawithsand@gmail.com>
2022-05-10 12:50:43 +0000

    Improved ButtonCollapse util, so it works and has defaultLabel option

--- src/components/util/collapse/ButtonCollapse.tsx
+++ src/components/util/collapse/ButtonCollapse.tsx
@@ -2,18 +2,35 @@ import React, { useState } from "react"
 import { Collapse } from "react-bootstrap"
 import { Transition } from "react-transition-group"
 
-const ButtonDropdown = (props: {
-	shownLabel?: string
-	hiddenLabel?: string
-	className?: string
-	children?: React.ReactElement & React.RefAttributes<Transition<any>>
-}) => {
-	const { children, shownLabel, hiddenLabel, className } = props
-	const [isShow, setIsShow] = useState(false)
+const ButtonDropdown = (
+	props: {
+		className?: string
+		children?: React.ReactElement & React.RefAttributes<Transition<any>>
+		defaultShown?: boolean
+	} & (
+		| { shownLabel: string; hiddenLabel: string; defaultLabel?: undefined }
+		| {
+				defaultLabel: string
+				shownLabel?: undefined
+				hiddenLabel?: undefined
+		  }
+	),
+) => {
+	const {
+		children,
+		shownLabel,
+		hiddenLabel,
+		className,
+		defaultShown,
+		defaultLabel,
+	} = props
+	const [isShow, setIsShow] = useState(defaultShown ?? false)
 	return (
 		<>
 			<button onClick={() => setIsShow(!isShow)} className={className}>
-				{isShow ? shownLabel : hiddenLabel}
+				{isShow
+					? shownLabel ?? defaultLabel
+					: hiddenLabel ?? defaultLabel}
 			</button>
 			<Collapse in={isShow}>{children as any}</Collapse>
 		</>
