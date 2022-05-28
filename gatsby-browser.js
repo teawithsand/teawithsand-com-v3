c38f9daf65f169d7a200d218a986cd3614aadbc5
teawithsand <teawithsand@gmail.com>
2022-05-28 15:30:13 +0000

    Implemented bunch of things required for sw to work

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,3 +10,18 @@ import "typeface-montserrat";
 
 
 import "./src/styles/global.scss";
+
+// TODO(teawithsand): configure gatsby-plugin-offline here with custom code in service worker
+
+export const onServiceWorkerUpdateReady = () => {
+	const answer = window.confirm(
+		`This application has been updated. ` +
+			`Reload to display the latest version?`,
+	)
+
+	if (answer === true) {
+		window.location.reload()
+	}
+}
+
+export const registerServiceWorker = () => true
