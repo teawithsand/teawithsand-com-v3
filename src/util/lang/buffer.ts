9c1efe688e99223f6dac4f68597917e1b9c44cdb
teawithsand <teawithsand@gmail.com>
2022-05-30 08:19:50 +0000

    Implemented util for generating random bytes using webcrypto api

--- src/util/lang/buffer.ts
+++ src/util/lang/buffer.ts
@@ -6,3 +6,20 @@ export const arrayBufferFromBytes = (bytes: number[]): ArrayBuffer => {
 	}
 	return b
 }
+
+export const randomBytesSync = (size: number): ArrayBuffer => {
+	const MAX_LEN = 1024
+
+	const buffer = new ArrayBuffer(size)
+	let offset = 0
+	for (;;) {
+		if (offset === buffer.byteLength) break
+
+		const sz = Math.min(MAX_LEN, buffer.byteLength - offset)
+		const arr = new Uint8Array(buffer.slice(offset, offset + sz))
+
+		crypto.getRandomValues(arr)
+		offset += sz
+	}
+	return buffer
+}
