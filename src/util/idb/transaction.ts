2b819307897e9bd8323d7489684830e75f3dd123
teawithsand <teawithsand@gmail.com>
2022-05-30 09:51:35 +0000

    Added some more utils for transaction management

--- src/util/idb/transaction.ts
+++ src/util/idb/transaction.ts
@@ -1,27 +1,57 @@
+import { unwrap } from "idb/with-async-ittr";
+
+
 type Base = {
 	done: Promise<unknown>
 	commit: () => void
 	abort: () => void
 }
 
+export function preventTransactionCloseOnError<T>(
+	promise: Promise<T>,
+): Promise<T> {
+	const request = unwrap(promise)
+	request.addEventListener("error", event => {
+		event.preventDefault()
+		event.stopPropagation()
+	})
+	return promise
+}
+
 export const useIDBPTransaction = async <T extends Base, E>(
 	tx: T,
 	cb: (tx: T) => Promise<E>,
 ): Promise<E> => {
+	let isDone = false
+	const done = tx.done
+		.catch(e => {
+			isDone = true
+			return Promise.reject(e)
+		})
+		.then(v => {
+			isDone = true
+			return Promise.resolve(v)
+		})
+
 	let res: E
+
 	try {
 		res = await cb(tx)
 	} catch (e) {
 		try {
-			tx.abort()
+			console.log("maybe abort", isDone)
+			if (!isDone) tx.abort()
 		} catch (e) {
 			// ignore it
 		}
 		throw e
 	}
-	tx.commit()
-
-	await tx.done
+	try {
+		if (!isDone) tx.commit()
+		await done
+	} catch (e) {
+		//ignore
+	}
 	return res
 }
 
@@ -49,8 +79,8 @@ export const useIDBPTransactionAbortOnly = async <T extends Base, E>(
 	try {
 		await tx.done
 	} catch (e) {
-        // ignore, it may have yield abort error
-        // which we are not interested in
-    }
+		// ignore, it may have yield abort error
+		// which we are not interested in
+	}
 	return res
 }
