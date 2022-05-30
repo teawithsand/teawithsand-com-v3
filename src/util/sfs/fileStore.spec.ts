7032be260627a11ca356e56a07c45af0e5a2f5c0
teawithsand <teawithsand@gmail.com>
2022-05-30 11:20:41 +0000

    Added more tests for file stores

--- src/util/sfs/fileStore.spec.ts
+++ src/util/sfs/fileStore.spec.ts
@@ -1,12 +1,11 @@
-import { concatArrayBuffers } from "@app/util/lang/arrayBuffer";
-import { collectAsyncIterable } from "@app/util/lang/asyncIterator";
-import { arrayBufferFromBytes, randomBytesSync } from "@app/util/lang/buffer";
-import { iterateOverReader } from "@app/util/lang/readableStream";
-import FileStore from "@app/util/sfs/FileStore";
-import FilesDB from "@app/util/sfs/idb/FilesDB";
-import IndexedDBFileStore from "@app/util/sfs/idb/IndexedDBFileStore";
-import InMemoryFileStore from "@app/util/sfs/InMemoryFileStore";
-
+import { concatArrayBuffers } from "@app/util/lang/arrayBuffer"
+import { collectAsyncIterable } from "@app/util/lang/asyncIterator"
+import { arrayBufferFromBytes, randomBytesSync } from "@app/util/lang/buffer"
+import { iterateOverReader } from "@app/util/lang/readableStream"
+import FileStore from "@app/util/sfs/FileStore"
+import FilesDB from "@app/util/sfs/idb/FilesDB"
+import IndexedDBFileStore from "@app/util/sfs/idb/IndexedDBFileStore"
+import InMemoryFileStore from "@app/util/sfs/InMemoryFileStore"
 
 const testFileStore = <T extends FileStore>(
 	name: string,
@@ -23,6 +22,13 @@ const testFileStore = <T extends FileStore>(
 			await cleanup(fileStore)
 		})
 
+		const writeSampleFile = async (path: string, data?: ArrayBuffer) => {
+			data = data ?? new ArrayBuffer(0)
+			const writer = (await fileStore.write(path)).getWriter()
+			await writer.write(data)
+			await writer.close()
+		}
+
 		it.each([
 			[[]],
 			[[arrayBufferFromBytes([10, 20, 30])]],
@@ -34,10 +40,14 @@ const testFileStore = <T extends FileStore>(
 					randomBytesSync(10),
 				],
 			],
+			[[randomBytesSync(1024 * 128 + 1), randomBytesSync(1024 * 128)]],
 			[
 				[
 					randomBytesSync(1024 * 128 + 1),
-					randomBytesSync(1024 * 128),
+					randomBytesSync(1024 * 88),
+					randomBytesSync(1024 * 12),
+					randomBytesSync(1024 * 22),
+					randomBytesSync(1024 * 91),
 				],
 			],
 		])("can store and read file", async chunks => {
@@ -61,6 +71,46 @@ const testFileStore = <T extends FileStore>(
 				await reader.cancel()
 			}
 		})
+
+		it("can delete file", async () => {
+			await writeSampleFile("/asdf.txt")
+			await fileStore.delete("/asdf.txt")
+
+			const res = await fileStore.stat("/asdf.txt")
+			expect(res).toStrictEqual(null)
+		})
+
+		it.each([
+			[[], [], ""],
+			[["/asdf.txt"], ["/asdf.txt"], "/"],
+			[["/asdf.txt", "/fdsa.txt", "/some/file.txt"], null, "/"],
+			[
+				["/asdf.txt", "fdsa.txt", "/some/file.txt"],
+				["/asdf.txt", "/fdsa.txt", "/some/file.txt"],
+				"/",
+			],
+			[
+				[
+					"/not-in-dir",
+					"/not-in-dir-2",
+					"/dir/in-dir",
+					"/dir/in-dir-2",
+				],
+				["/dir/in-dir", "/dir/in-dir-2"],
+				"/dir/",
+			],
+		])("can list files", async (files, expected, prefix) => {
+			for (const f of files) {
+				await writeSampleFile(f)
+			}
+
+			if (!expected) expected = files
+
+			const results = await collectAsyncIterable(fileStore.list(prefix))
+			results.sort()
+			expected.sort()
+			expect(results).toEqual(expected)
+		})
 	})
 }
 
@@ -72,6 +122,7 @@ testFileStore(
 	},
 )
 
+/*
 testFileStore(
 	"IndexedDBFileStore",
 	async () => {
@@ -84,3 +135,4 @@ testFileStore(
 		await res.disownFilesDB().close()
 	},
 )
+*/
