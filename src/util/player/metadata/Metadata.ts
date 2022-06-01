adb7b56ce7b87e90c2072ff68024f2b2bf14067f
teawithsand <teawithsand@gmail.com>
2022-06-01 11:58:34 +0000

    Added MetadataLoadingResult type

--- src/util/player/metadata/Metadata.ts
+++ src/util/player/metadata/Metadata.ts
@@ -2,4 +2,19 @@ type Metadata = {
 	duration: number | null
 }
 
+export enum MetadataLoadingResultType {
+	OK = 1,
+	ERROR = 2,
+}
+
+export type MetadataLoadingResult =
+	| {
+			type: MetadataLoadingResultType.OK
+			metadata: Metadata
+	  }
+	| {
+			type: MetadataLoadingResultType.ERROR
+			error: any
+	  }
+
 export default Metadata
