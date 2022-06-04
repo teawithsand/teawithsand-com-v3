a25a132587123fd845adca097bce77b897fc969e
teawithsand <teawithsand@gmail.com>
2022-06-04 12:15:37 +0000

    Fixed MetadataTool bug, so it has timeout and actually loads metadata now

--- src/util/player/metadata/MetadataTool.ts
+++ src/util/player/metadata/MetadataTool.ts
@@ -9,24 +9,26 @@ export default class MetadataTool {
 					release: () => void
 			  },
 	): Promise<Metadata> => {
-		let url
+		let url: string
 		if (typeof src === "string") {
 			url = src
 		} else {
 			url = src.url
 		}
+
 		try {
 			const audio = new Audio()
-			audio.src = url
-			audio.preload = "metadata"
-			audio.volume = 0
-			audio.pause()
 
 			let errorListener: () => void
 			let metadataListener: () => void
 			const p = new Promise<Metadata>((resolve, reject) => {
 				errorListener = () => {
-					reject(audio.error)
+					reject(
+						audio.error ??
+							new Error(
+								`Unknown error when loading metadata for ${url}`,
+							),
+					)
 				}
 				metadataListener = () => {
 					let duration: number | null = audio.duration
@@ -35,6 +37,26 @@ export default class MetadataTool {
 					resolve({
 						duration,
 					})
+				}
+
+				audio.addEventListener("error", errorListener)
+				audio.addEventListener("stalled", errorListener)
+				audio.addEventListener("loadedmetadata", metadataListener)
+
+				const timeout = setTimeout(() => {
+					reject(
+						new Error(`Timeout when loading metadata for ${url}`),
+					)
+				}, 30 * 1000) // 30s is a lot of timeout btw
+
+				try {
+					audio.src = url
+					audio.preload = "metadata"
+					audio.volume = 0
+					audio.pause()
+					audio.load()
+				} finally {
+					clearTimeout(timeout)
 				}
 			}).finally(() => {
 				audio.removeEventListener("error", errorListener)
