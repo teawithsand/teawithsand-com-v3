1a025792cbb1d74fb99f50b856816d95d7760cc3
teawithsand <teawithsand@gmail.com>
2022-06-06 15:28:43 +0000

    Added delete method to key-value store

--- src/util/keyvalue/LocalForageKeyValueStore.ts
+++ src/util/keyvalue/LocalForageKeyValueStore.ts
@@ -1,22 +1,27 @@
 import KeyValueStore from "@app/util/keyvalue/KeyValueStore"
 
-export default class LocalForageKeyValueStore<
-	V,
-	K extends string = string,
-> implements KeyValueStore<V, K>
+export default class LocalForageKeyValueStore<V, K extends string = string>
+	implements KeyValueStore<V, K>
 {
 	constructor(private readonly forage: LocalForage) {}
+
+	delete = async (id: K): Promise<void> => {
+		await this.forage.removeItem(id)
+	}
 
 	get = async (id: K): Promise<V | null> => {
 		const res = await this.forage.getItem(id)
 		return res as V | null
 	}
+	
 	set = async (id: K, value: V) => {
 		await this.forage.setItem(id, value)
 	}
+
 	clear = async (): Promise<void> => {
 		await this.forage.clear()
 	}
+
 	iterateKeys = (): AsyncIterable<K> => {
 		const { forage } = this
 		async function* gen() {
