166324356654933cb59340cd8fa2044e658a451c
teawithsand <teawithsand@gmail.com>
2022-06-07 18:29:35 +0000

    Linked tws-common package into blog one and fixed minor bug causing type definitions not to be emited in tws-common

--- blog/src/pages/index.tsx
+++ blog/src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { graphql } from "gatsby"
 import React from "react"
+import { helloWorld } from "tws-common"
 
 import Layout from "@app/components/layout/Layout"
 import Home from "@app/components/pages/home/home"
 
 const Index = () => {
+	helloWorld()
 	return (
 		<Layout>
 			<Home />
