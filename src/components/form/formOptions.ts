72e4ee9188b66f15e2a9a7657cebac8d1a845e89
teawithsand <teawithsand@gmail.com>
2022-06-07 14:55:48 +0000

    Removed parts of half-baked form stuff

--- src/components/form/formOptions.ts
+++
@@ -1,6 +0,0 @@
-export type FormProps<T> = {
-	onChange?: (value: T) => void
-	validate?: (value: T) => { [key: string]: string } | null
-	validateAsync?: (value: T) => Promise<{ [key: string]: string } | null>
-	onSubmit?: (value: T) => void
-}
