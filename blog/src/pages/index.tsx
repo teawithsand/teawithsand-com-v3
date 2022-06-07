6dd5847abadf02c76fbef400098e13490654d7d5
teawithsand <teawithsand@gmail.com>
2022-06-07 18:30:40 +0000

    Removed call to hello world function from homepage

--- blog/src/pages/index.tsx
+++ blog/src/pages/index.tsx
@@ -1,12 +1,10 @@
 import { graphql } from "gatsby"
 import React from "react"
-import { helloWorld } from "tws-common"
 
 import Layout from "@app/components/layout/Layout"
 import Home from "@app/components/pages/home/home"
 
 const Index = () => {
-	helloWorld()
 	return (
 		<Layout>
 			<Home />
