dc7d03790564b87339305e289ca8e9c1be3145d2
teawithsand <teawithsand@gmail.com>
2022-06-08 12:14:54 +0000

    Added util for getting sorted list of async iterable results

--- tws-common/src/lang/asyncIterator.ts
+++ tws-common/src/lang/asyncIterator.ts
@@ -8,6 +8,22 @@ export const collectAsyncIterable = async <T>(
 	return arr
 }
 
+/**
+ * It's surprising how often such function becomes handy.
+ */
+export const collectAndSortAsyncIterable = async <T>(
+	iterable: AsyncIterable<T>,
+	comparator?: (a: T, b: T) => number,
+): Promise<T[]> => {
+	const arr = []
+	for await (const v of iterable) {
+		arr.push(v)
+	}
+
+	arr.sort(comparator)
+	return arr
+}
+
 export async function* makeAsyncIterable<T>(iterable: Iterable<T>) {
 	for (const e of iterable) {
 		yield e
