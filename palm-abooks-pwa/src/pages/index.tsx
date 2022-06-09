05a9901469a415e4521af2de6c3bc854ea84fb97
teawithsand <teawithsand@gmail.com>
2022-06-09 20:03:28 +0000

    Cleaned up index page

--- palm-abooks-pwa/src/pages/index.tsx
+++ palm-abooks-pwa/src/pages/index.tsx
@@ -1,11 +1,9 @@
 import CreateABookForm from "@app/components/abook/CreateABookForm"
 import React from "react"
-import NavBar from "tws-common/ui/material/component/NavBar"
 
 const IndexPage = () => {
 	return (
 		<div>
-			<NavBar />
 			<CreateABookForm />
 		</div>
 	)
