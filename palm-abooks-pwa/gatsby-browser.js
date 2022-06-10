0623c7ba138713cefcd707960548c492150b2816
teawithsand <teawithsand@gmail.com>
2022-06-10 07:00:21 +0000

    Some minor improvements to make common package work

--- palm-abooks-pwa/gatsby-browser.js
+++ palm-abooks-pwa/gatsby-browser.js
@@ -1,5 +1,7 @@
 // TODO(teawithsand): configure gatsby-plugin-offline here with custom code in service worker
 
+import "tws-common/scss/ui.scss"
+
 export const onServiceWorkerUpdateReady = () => {
 	const answer = window.confirm(
 		`This application has been updated. ` +
--- palm-abooks-pwa/src/pages/index.tsx
+++ palm-abooks-pwa/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import CreateABookForm from "@app/components/abook/CreateABookForm"
 import React from "react"
+import { Button } from "tws-common/dist/ui"
 
 const IndexPage = () => {
 	return (
