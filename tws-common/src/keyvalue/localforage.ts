2abe2e751e647a38622ec998fe792467144772e8
teawithsand <teawithsand@gmail.com>
2022-06-11 07:29:29 +0000

    Added additional missing exports of localforage

--- tws-common/src/keyvalue/localforage.ts
+++ tws-common/src/keyvalue/localforage.ts
@@ -1,5 +1,7 @@
-import localforage from "localforage"
+import localforage, { INDEXEDDB, LOCALSTORAGE, WEBSQL } from "localforage"
 import { extendPrototype } from "localforage-startswith"
+
+export { INDEXEDDB, LOCALSTORAGE, WEBSQL }
 
 export default extendPrototype(localforage)
 export type LocalForage = ReturnType<typeof extendPrototype>
