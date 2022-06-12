3d84d9f5ec3a8d4b682015e01877c0649fe3f94c
teawithsand <teawithsand@gmail.com>
2022-06-12 15:54:53 +0000

    Fixed weird problem with extendPrototype method of localforage-startswith, which returned undefined for some reason

--- tws-common/src/keyvalue/localforage.ts
+++ tws-common/src/keyvalue/localforage.ts
@@ -1,7 +1,21 @@
-import localforage, { INDEXEDDB, LOCALSTORAGE, WEBSQL } from "localforage"
+import * as localforage from "localforage"
+import { INDEXEDDB, LOCALSTORAGE, WEBSQL } from "localforage"
 import { extendPrototype } from "localforage-startswith"
 
 export { INDEXEDDB, LOCALSTORAGE, WEBSQL }
 
-export default extendPrototype(localforage)
-export type LocalForage = ReturnType<typeof extendPrototype>
+Object.setPrototypeOf(localforage, {})
+extendPrototype(localforage)
+const newLocalForage: ReturnType<typeof extendPrototype> = localforage
+
+if (typeof Object.getPrototypeOf({}).startsWith !== "undefined") {
+	throw new Error(
+		"object prototype was polluted by extendPrototype from localforage-startswith",
+	)
+}
+if (typeof localforage.startsWith !== "function") {
+	throw new Error("expected startsWith, but it's not there")
+}
+
+export default newLocalForage
+export type LocalForage = typeof newLocalForage
