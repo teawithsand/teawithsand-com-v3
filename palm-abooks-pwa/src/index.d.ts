948f643b95ca226f4d7ccd9d9c8f65660895718e
teawithsand <teawithsand@gmail.com>
2022-06-13 09:32:16 +0000

    Added index.d.ts file to palm-abooks-pwa for svg and scss imports

---
+++ palm-abooks-pwa/src/index.d.ts
@@ -0,0 +1,29 @@
+declare module "*.module.scss"
+declare module "*.svg" {
+	export default React.PureComponent
+}
+
+declare const __PATH_PREFIX__: string
+
+declare module "*.apk" {
+	const fooUrl: string
+	namespace fooUrl {}
+	export = fooUrl
+}
+/*
+
+declare module 'service-worker-loader!*' {
+    const register: import('service-worker-loader/types').ServiceWorkerRegister;
+    const ServiceWorkerNoSupportError: import('service-worker-loader/types').ServiceWorkerNoSupportError;
+    const scriptUrl: import('service-worker-loader/types').ScriptUrl;
+    export default register;
+    export {
+        ServiceWorkerNoSupportError,
+        scriptUrl
+    };
+}
+// or, for example
+declare module '*?sw' {
+    // ...
+}
+*/
