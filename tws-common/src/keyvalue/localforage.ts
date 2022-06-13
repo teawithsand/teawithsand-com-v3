1509e962f2f122d285f7e89d10c0b9249be7a79b
teawithsand <teawithsand@gmail.com>
2022-06-13 09:26:11 +0000

    Fixed localforage no prototype import issue

--- tws-common/src/keyvalue/localforage.ts
+++ tws-common/src/keyvalue/localforage.ts
@@ -1,21 +1,25 @@
-import * as localforage from "localforage"
-import { INDEXEDDB, LOCALSTORAGE, WEBSQL } from "localforage"
-import { extendPrototype } from "localforage-startswith"
+import { default as localforage } from "localforage";
+import { INDEXEDDB, LOCALSTORAGE, WEBSQL } from "localforage";
+import { extendPrototype } from "localforage-startswith";
 
-export { INDEXEDDB, LOCALSTORAGE, WEBSQL }
 
-Object.setPrototypeOf(localforage, {})
 extendPrototype(localforage)
-const newLocalForage: ReturnType<typeof extendPrototype> = localforage
+const newLocalForage = localforage as ReturnType<typeof extendPrototype>
+
+if (typeof newLocalForage !== "object" || newLocalForage === null) {
+	throw new Error("newLocalForage is not object!")
+}
 
 if (typeof Object.getPrototypeOf({}).startsWith !== "undefined") {
 	throw new Error(
 		"object prototype was polluted by extendPrototype from localforage-startswith",
 	)
 }
-if (typeof localforage.startsWith !== "function") {
+
+if (typeof newLocalForage.startsWith !== "function") {
 	throw new Error("expected startsWith, but it's not there")
 }
 
+export { INDEXEDDB, LOCALSTORAGE, WEBSQL }
 export default newLocalForage
 export type LocalForage = typeof newLocalForage
