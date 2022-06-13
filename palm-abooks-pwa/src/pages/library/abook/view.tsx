9dceb877741dec0ff950d585f5fd97842a2335bc
teawithsand <teawithsand@gmail.com>
2022-06-13 11:08:31 +0000

    Added very basic and untested abook view

--- palm-abooks-pwa/src/pages/library/abook/view.tsx
+++ palm-abooks-pwa/src/pages/library/abook/view.tsx
@@ -1,10 +1,66 @@
+import PageContainer from "@app/components/layout/PageContainer"
+import LoadingSpinner from "@app/components/shared/loading-spinner/LoadingSpinner"
 import { useABookStore } from "@app/domain/abook/ABookStore"
-import React from "react"
+import React, { ReactFragment, ReactNode } from "react"
+import { useQuery } from "tws-common/react/hook/query"
+import { useGetParamsObject } from "tws-common/react/hook/useGetParams"
+import { Col, Row } from "tws-common/ui"
 
 const ABookView = () => {
 	const store = useABookStore()
+	const { id } = useGetParamsObject()
 
-	return <div>NIY</div>
+	const {
+		data: abookAR,
+		isLoading,
+		isError,
+	} = useQuery(["abook-view-get-abook", id], async () => {
+		const abookAR = await store.get(id ?? "")
+		return abookAR
+	})
+
+	let inner: ReactFragment | ReactNode = null
+	if (isLoading) {
+		inner = (
+			<>
+				<LoadingSpinner />
+			</>
+		)
+	} else if (isError) {
+		inner = (
+			<div>
+				An error occurred while loading ABook with id `{id ?? ""}`
+			</div>
+		)
+	} else if (abookAR) {
+		inner = (
+			<>
+				<Row>
+					<Col>
+						<h1>{abookAR.metadata.title}</h1>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<p>{abookAR.metadata.description}</p>
+					</Col>
+				</Row>
+			</>
+		)
+	} else {
+		inner = (
+			<>
+				<Row>
+					{/* TODO(teawithsand): make this not-found-generic-component */}
+					<Col>
+						<h1>No such ABook exists. Go back.</h1>
+					</Col>
+				</Row>
+			</>
+		)
+	}
+
+	return <PageContainer>{inner}</PageContainer>
 }
 
 export default ABookView
