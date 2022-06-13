997c19d5bdfd36cbe651b5e5b89953f5a7a6b90e
teawithsand <teawithsand@gmail.com>
2022-06-13 10:55:15 +0000

    Improved page container to accept ReactNode along with ReactFragment

--- palm-abooks-pwa/src/components/layout/PageContainer.tsx
+++ palm-abooks-pwa/src/components/layout/PageContainer.tsx
@@ -1,7 +1,7 @@
-import React, { ReactFragment } from "react"
+import React, { ReactFragment, ReactNode } from "react"
 import { Container as ParentContainer } from "tws-common/ui"
 
-const PageContainer = (props: { children?: ReactFragment }) => {
+const PageContainer = (props: { children?: ReactFragment | ReactNode }) => {
 	return <ParentContainer className="mt-5">{props.children}</ParentContainer>
 }
 export default PageContainer
