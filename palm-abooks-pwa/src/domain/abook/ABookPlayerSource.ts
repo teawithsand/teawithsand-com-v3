363b3030592431790af8f8479e368bce977a0535
teawithsand <teawithsand@gmail.com>
2022-06-15 15:39:35 +0000

    AddedABookPlayerSource, which uses ABookActiveRecord to handle loading files

---
+++ palm-abooks-pwa/src/domain/abook/ABookPlayerSource.ts
@@ -0,0 +1,34 @@
+import { ABookActiveRecord } from "@app/domain/abook/ABookStore"
+import PlayerSource, {
+	LoadingPlayerSource,
+	LoginPlayerSourceContent,
+} from "tws-common/player/source/PlayerSource"
+import PlayerSourceError from "tws-common/player/source/PlayerSourceError"
+
+export default class ABookPlayerSource extends LoadingPlayerSource {
+	public readonly id: string
+	constructor(
+		public readonly abookActiveRecord: ABookActiveRecord,
+		public readonly sourceId: string,
+	) {
+		super()
+		this.id = `aab:${abookActiveRecord.id}.${sourceId}`
+	}
+
+	loadTarget = async (): Promise<LoginPlayerSourceContent> => {
+		const sfo = await this.abookActiveRecord.files.getFile(this.sourceId)
+		if (!sfo)
+			throw new PlayerSourceError(
+				`Source with ${this.sourceId} was not fond on abook with id ${this.abookActiveRecord.id}`,
+			)
+
+		return {
+			type: "urlObject",
+			data: sfo.innerObject,
+		}
+	}
+
+	equals = (b: PlayerSource): boolean => {
+		return b instanceof ABookPlayerSource && b.id === this.id
+	}
+}
