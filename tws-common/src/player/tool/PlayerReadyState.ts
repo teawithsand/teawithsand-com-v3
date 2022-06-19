229b40e9affa851eb34c840fed9a8c8f746ecbd8
teawithsand <teawithsand@gmail.com>
2022-06-19 12:34:01 +0000

    Refactored PlayerNetworkState and PlayerReadyState

--- tws-common/src/player/tool/PlayerReadyState.ts
+++ tws-common/src/player/tool/PlayerReadyState.ts
@@ -1,23 +1,23 @@
 enum PlayerReadyState {
-	NOTHING,
-	METADATA,
-	CURRENT_DATA,
-	FUTURE_DATA,
-	ENOUGH_DATA,
+	NOTHING = 0,
+	METADATA = 1,
+	CURRENT_DATA = 2,
+	FUTURE_DATA = 3,
+	ENOUGH_DATA = 4,
 }
 
 export const simplePlayerReadyStateFromNative = (
 	n: number,
 ): PlayerReadyState => {
-	if (n == 0) {
+	if (n === 0) {
 		return PlayerReadyState.NOTHING
-	} else if (n == 1) {
+	} else if (n === 1) {
 		return PlayerReadyState.METADATA
-	} else if (n == 2) {
+	} else if (n === 2) {
 		return PlayerReadyState.CURRENT_DATA
-	} else if (n == 3) {
+	} else if (n === 3) {
 		return PlayerReadyState.FUTURE_DATA
-	} else if (n == 4) {
+	} else if (n === 4) {
 		return PlayerReadyState.ENOUGH_DATA
 	} else {
 		return PlayerReadyState.NOTHING // actually it should throw imho
