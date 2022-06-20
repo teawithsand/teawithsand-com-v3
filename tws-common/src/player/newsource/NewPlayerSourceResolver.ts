c0ee855f8c3f0a77df0f4faa8d8def1c0a6a0f73
teawithsand <teawithsand@gmail.com>
2022-06-20 17:53:07 +0000

    Ported previous source resolver for new source implementation

--- tws-common/src/player/newsource/NewPlayerSourceResolver.ts
+++ tws-common/src/player/newsource/NewPlayerSourceResolver.ts
@@ -6,3 +6,113 @@ export type NewPlayerSourceResolver<T extends NewPlayerSource> = {
 	 */
 	resolveSourceToURL(source: T): [string, () => void]
 }
+
+export abstract class NewPlayerSourceResolverImpl<T extends NewPlayerSource>
+	implements NewPlayerSourceResolver<T>
+{
+	private readonly cachedSourcesIds: Map<
+		string,
+		{
+			url: string
+			release: () => void
+			counter: number
+		}
+	> = new Map()
+
+	private getURLCached = (id: string, url: string): [string, () => void] => {
+		if (!this.cachedSourcesIds.has(id)) {
+			this.cachedSourcesIds.set(id, {
+				counter: 1,
+				release: () => {
+					// noop, we do not release URLs
+				},
+				url,
+			})
+		}
+
+		const cached = this.cachedSourcesIds.get(id)
+		if (!cached) throw new Error("unreachable code")
+
+		cached.counter += 1
+		let closed = false
+		return [
+			cached.url,
+			() => {
+				if (!closed) {
+					cached.counter -= 1
+					closed = true
+
+					if (cached.counter === 0) {
+						cached.release()
+						this.cachedSourcesIds.delete(id)
+					}
+				}
+			},
+		]
+	}
+
+	private getCachedBlobURL = (
+		id: string,
+		blob: Blob | File | MediaSource,
+	): [string, () => void] => {
+		if (!this.cachedSourcesIds.has(id)) {
+			const url = URL.createObjectURL(blob)
+			this.cachedSourcesIds.set(id, {
+				counter: 1,
+				release: () => {
+					URL.revokeObjectURL(url)
+				},
+				url,
+			})
+		}
+
+		const cached = this.cachedSourcesIds.get(id)
+		if (!cached) throw new Error("unreachable code")
+
+		cached.counter += 1
+		let closed = false
+		return [
+			cached.url,
+			() => {
+				if (!closed) {
+					cached.counter -= 1
+					closed = true
+
+					if (cached.counter === 0) {
+						cached.release()
+						this.cachedSourcesIds.delete(id)
+					}
+				}
+			},
+		]
+	}
+
+	protected abstract extractData(source: T): (
+		| {
+				type: "blob"
+				blob: Blob
+		  }
+		| {
+				type: "url"
+				url: string
+		  }
+	) & {
+		id: string
+	}
+
+	resolveSourceToURL = (source: T): [string, () => void] => {
+		const data = this.extractData(source)
+		if (data.type === "url") {
+			return [
+				data.url,
+				() => {
+					// noop
+				},
+			]
+		} else if (data.type === "blob") {
+			return this.getCachedBlobURL(data.id, data.blob)
+		} else {
+			throw new Error("unreachable code")
+		}
+	}
+}
