ce4071abbf3fc41273345bcc06b1777384b41180
teawithsand <teawithsand@gmail.com>
2022-06-21 06:23:52 +0000

    Cleaned up old metadata loading testing code

--- palm-abooks-pwa/src/components/layout/Layout.tsx
+++ palm-abooks-pwa/src/components/layout/Layout.tsx
@@ -8,7 +8,6 @@ import { createStore } from "@app/domain/redux/store"
 
 import { BFRMetadataLoader } from "tws-common/player/bfr/metadataLoader"
 import { BFRPlayer } from "tws-common/player/bfr/player"
-import DefaultMetadataLoader from "tws-common/player/metadata/loader/DefaultMetadataLoader"
 import { QueryClient, QueryClientProvider } from "tws-common/react/hook/query"
 
 const queryClient = new QueryClient()
@@ -43,33 +42,6 @@ const Layout = (props: any) => {
 		}
 	}, [])
 
-	/*
-	useEffect(() => {
-		let called = false
-		const clean = store.subscribe(() => {
-			if (called) return
-			const state = store.getState()
-
-			const loader = new DefaultMetadataLoader(
-				MPlayerSourceResolver.getInstance(),
-			)
-
-			const sources =
-				state.bfrState.playerConfig.playlist.data?.sources ?? []
-			const source = sources.length > 0 ? sources[0] : null
-			console.error("GOT SOURCE", source)
-			if (source) {
-				called = true
-				loader
-					.loadMetadata(source)
-					.then(console.error)
-					.catch(console.error)
-			}
-		})
-		return () => clean()
-	}, [])
-
-	*/
 	return (
 		<>
 			<Provider store={store}>
