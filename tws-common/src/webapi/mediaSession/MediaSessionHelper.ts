9bdd2cdfec59672c2358e390052faabc8418e16a
teawithsand <teawithsand@gmail.com>
2022-06-22 08:28:10 +0000

    Added variable media session capabilities feature to MediaSessionHelper

--- tws-common/src/webapi/mediaSession/MediaSessionHelper.ts
+++ tws-common/src/webapi/mediaSession/MediaSessionHelper.ts
@@ -1,6 +1,5 @@
-import { SimpleEventBus } from "tws-common/lang/bus/EventBus";
-import { Subscribable } from "tws-common/lang/bus/stateSubscribe";
-
+import { SimpleEventBus } from "tws-common/lang/bus/EventBus"
+import { Subscribable } from "tws-common/lang/bus/stateSubscribe"
 
 export type MediaSessionMetadata = {
 	title: string
@@ -13,6 +12,12 @@ export type MediaSessionMetadata = {
 	}[]
 }
 
+export type MediaSessionPositionState = {
+	duration: number | null // if null then inf stream
+	position: number // may not be greater than duration, if it's not null
+	playbackRate: number // must be > 0
+}
+
 export enum MediaSessionEventType {
 	PLAY = 1,
 	PAUSE = 2,
@@ -23,12 +28,6 @@ export enum MediaSessionEventType {
 	PREVIOUS_TRACK = 7,
 	NEXT_TRACK = 8,
 	SKIP_AD = 9,
-}
-
-export type PositionState = {
-	duration: number | null // if null then inf stream
-	position: number // may not be greater than duration, if it's not null
-	playbackRate: number // must be > 0
 }
 
 export type MediaSessionEvent =
@@ -64,18 +63,30 @@ export type MediaSessionEvent =
 			type: MediaSessionEventType.SKIP_AD
 	  }
 
-export type MediaSessionPositionState = {
-	duration?: number
-	playbackRate?: number
-	position?: number
-}
 // Docs:
 // https://developer.mozilla.org/en-US/docs/Web/API/MediaSession/setActionHandler
 // https://developer.mozilla.org/en-US/docs/Web/API/MediaSession/setPositionState
 
+export type MediaSessionActionType =
+	| "play"
+	| "pause"
+	| "stop"
+	| "seekbackward"
+	| "seekforward"
+	| "seekto"
+	| "previoustrack"
+	| "nexttrack"
+	| "skipad"
+
+const tuplesToMap = <T, E>(iterable: Iterable<[T, E]>): Map<T, E> => {
+	const m = new Map()
+	for (const [k, v] of iterable) m.set(k, v)
+	return m
+}
+
 class MediaSessionHelperImpl {
 	private checkSupport = () =>
-		window.navigator && window.navigator.mediaSession
+		window && window.navigator && window.navigator.mediaSession
 
 	private innerEventBus = new SimpleEventBus<MediaSessionEvent>()
 
@@ -85,67 +96,129 @@ class MediaSessionHelperImpl {
 
 	public readonly isSupported = this.checkSupport()
 
-	constructor() {
-		if (this.checkSupport()) {
-			window.navigator.mediaSession.setActionHandler("play", () =>
+	private supportedActions: Set<MediaSessionActionType> = new Set()
+
+	private readonly actionHandlers: Map<
+		MediaSessionActionType,
+		(e: any) => void
+	> = tuplesToMap([
+		[
+			"play",
+			() =>
 				this.innerEventBus.emitEvent({
 					type: MediaSessionEventType.PLAY,
 				}),
-			)
-			window.navigator.mediaSession.setActionHandler("pause", () =>
+		],
+		[
+			"pause",
+			() =>
 				this.innerEventBus.emitEvent({
 					type: MediaSessionEventType.PAUSE,
 				}),
-			)
-			window.navigator.mediaSession.setActionHandler("stop", () =>
+		],
+
+		[
+			"stop",
+			() =>
 				this.innerEventBus.emitEvent({
 					type: MediaSessionEventType.STOP,
 				}),
-			)
-			window.navigator.mediaSession.setActionHandler(
-				"seekbackward",
-				details =>
-					this.innerEventBus.emitEvent({
-						type: MediaSessionEventType.SEEK_BACKWARD,
-						by: details.seekOffset ?? null,
-					}),
-			)
-			window.navigator.mediaSession.setActionHandler(
-				"seekforward",
-				details =>
-					this.innerEventBus.emitEvent({
-						type: MediaSessionEventType.SEEK_FORWARD,
-						by: details.seekOffset ?? null,
-					}),
-			)
-
-			window.navigator.mediaSession.setActionHandler("seekto", details =>
+		],
+		[
+			"seekbackward",
+			(details: any) =>
+				this.innerEventBus.emitEvent({
+					type: MediaSessionEventType.SEEK_BACKWARD,
+					by: details.seekOffset ?? null,
+				}),
+		],
+		[
+			"seekforward",
+			(details: any) =>
+				this.innerEventBus.emitEvent({
+					type: MediaSessionEventType.SEEK_FORWARD,
+					by: details.seekOffset ?? null,
+				}),
+		],
+		[
+			"seekto",
+			(details: any) =>
 				this.innerEventBus.emitEvent({
 					type: MediaSessionEventType.SEEK_TO,
 					to: details.seekTime ?? null,
 					fastSeek: details.fastSeek ?? null,
 				}),
-			)
-			window.navigator.mediaSession.setActionHandler(
-				"previoustrack",
-				() =>
-					this.innerEventBus.emitEvent({
-						type: MediaSessionEventType.PREVIOUS_TRACK,
-					}),
-			)
-			window.navigator.mediaSession.setActionHandler("nexttrack", () =>
+		],
+		[
+			"previoustrack",
+			() =>
+				this.innerEventBus.emitEvent({
+					type: MediaSessionEventType.PREVIOUS_TRACK,
+				}),
+		],
+		[
+			"nexttrack",
+			() =>
 				this.innerEventBus.emitEvent({
 					type: MediaSessionEventType.NEXT_TRACK,
 				}),
-			)
-			/*
-			window.navigator.mediaSession.setActionHandler("skipad", () =>
+		],
+		[
+			"skipad",
+			() =>
 				this.innerEventBus.emitEvent({
 					type: MediaSessionEventType.SKIP_AD,
 				}),
-			)
-			*/
+		],
+	])
+
+	constructor() {
+		this.setSupportedActions([
+			"play",
+			"pause",
+			"stop",
+			"seekbackward",
+			"seekforward",
+			"seekto",
+			"previoustrack",
+			"nexttrack",
+			"skipad",
+		])
+	}
+
+	setSupportedActions = (
+		actions:
+			| MediaSessionActionType[]
+			| Iterable<MediaSessionActionType>
+			| Set<MediaSessionActionType>,
+	) => {
+		// make a copy + cast if needed
+		const castedActions = new Set(actions)
+
+		const removedActions = [...this.supportedActions.values()].filter(
+			v => !castedActions.has(v),
+		)
+
+		const addedActions = [...castedActions.values()].filter(
+			v => !this.supportedActions.has(v),
+		)
+
+		if (this.checkSupport()) {
+			for (const a of removedActions) {
+				navigator.mediaSession.setActionHandler(a, null)
+			}
+
+			for (const a of addedActions) {
+				const h = this.actionHandlers.get(a)
+				if (!h)
+					throw new Error(
+						`Unreachable code - no handler for action ${a}`,
+					)
+				navigator.mediaSession.setActionHandler(a, h)
+			}
 		}
+
+		this.supportedActions = castedActions
 	}
 
 	setMetadata = (metadata: MediaSessionMetadata) => {
@@ -157,8 +230,8 @@ class MediaSessionHelperImpl {
 		if (this.checkSupport()) navigator.mediaSession.playbackState = state
 	}
 
-	setPositionState = (state: PositionState) => {
-		if (this.checkSupport()) {
+	setPositionState = (state: MediaSessionPositionState) => {
+		if (this.checkSupport() && navigator.mediaSession.setPositionState) {
 			let duration = state.duration ?? Infinity
 			if (duration < 0) duration = 0
 
