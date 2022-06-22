1254f90bc394273b3ea545b572a65c3d1569fd1f
teawithsand <teawithsand@gmail.com>
2022-06-22 13:59:00 +0000

    Integrated BFR media session with palm-abooks-pwa in some basic way

--- palm-abooks-pwa/src/components/layout/Layout.tsx
+++ palm-abooks-pwa/src/components/layout/Layout.tsx
@@ -3,12 +3,17 @@ import { Provider } from "react-redux"
 
 import Navbar from "@app/components/layout/Navbar"
 import { MBFRMetadataLoaderAdapter } from "@app/domain/bfr/metadataLoader"
-import { MPlayerSourceResolver } from "@app/domain/bfr/source"
+import { MPlaylistMetadata } from "@app/domain/bfr/playlist"
+import { MPlayerSource, MPlayerSourceResolver } from "@app/domain/bfr/source"
 import { createStore } from "@app/domain/redux/store"
 
+import { setIsPlayingWhenReady } from "tws-common/player/bfr/actions"
+import { BFRMediaSession } from "tws-common/player/bfr/mediaSession"
 import { BFRMetadataLoader } from "tws-common/player/bfr/metadataLoader"
 import { BFRPlayer } from "tws-common/player/bfr/player"
+import { BFRPlaylist } from "tws-common/player/bfr/state"
 import { QueryClient, QueryClientProvider } from "tws-common/react/hook/query"
+import { MediaSessionEventType } from "tws-common/webapi/mediaSession/MediaSessionHelper"
 
 const queryClient = new QueryClient()
 
@@ -39,6 +44,51 @@ const Layout = (props: any) => {
 
 		return () => {
 			loader.release()
+		}
+	}, [])
+
+	useEffect(() => {
+		const mediaSession = new BFRMediaSession(
+			store,
+			state => state.bfrState,
+			{
+				handleMediaSessionEvent: e => {
+					if (e.type === MediaSessionEventType.PAUSE) {
+						store.dispatch(setIsPlayingWhenReady(false))
+					} else if (e.type === MediaSessionEventType.PLAY) {
+						store.dispatch(setIsPlayingWhenReady(true))
+					} else {
+						// NIY implement this
+					}
+				},
+				selectMetadata: (
+					playlist: BFRPlaylist<MPlaylistMetadata, MPlayerSource>,
+					i,
+				) => ({
+					title: "PalmABooks PWA playing",
+					album: "No album",
+					artist: "teawithsand :3",
+					artwork: [],
+				}),
+				selectPlayerState: state => {
+					if (state.bfrState.playerConfig.isPlayingWhenReady) {
+						return "playing"
+					} else {
+						return "paused"
+					}
+				},
+				selectPositionState: state => {
+					return {
+						duration: state.bfrState.playerState.duration,
+						playbackRate: state.bfrState.playerConfig.speed,
+						position: state.bfrState.playerState.position ?? 0,
+					}
+				},
+			},
+		)
+
+		return () => {
+			mediaSession.release()
 		}
 	}, [])
 
