ea455fdc8eae53e7b6977bc00c9653ffa4ae6890
teawithsand <teawithsand@gmail.com>
2022-06-23 18:55:59 +0000

    Integrated GlobalIDManager into ABook stores

--- palm-abooks-pwa/src/domain/abook/ABookDataStore.ts
+++ palm-abooks-pwa/src/domain/abook/ABookDataStore.ts
@@ -1,7 +1,9 @@
 import { ABookData } from "@app/domain/abook/ABookStore"
 
 import LocalForageKeyValueStore from "tws-common/keyvalue/LocalForageKeyValueStore"
+import { claimId, NS_STORE } from "tws-common/misc/GlobalIDManager"
 
-export const ABOOK_DATA_STORE =
-	LocalForageKeyValueStore.simple<ABookData>("abookMetadata")
+export const ABOOK_DATA_STORE = LocalForageKeyValueStore.simple<ABookData>(
+	claimId(NS_STORE, "palm-abooks-pwa/abookMetadata"),
+)
 export const useABookDataStore = () => ABOOK_DATA_STORE
--- palm-abooks-pwa/src/domain/abook/ABookFileStore.ts
+++ palm-abooks-pwa/src/domain/abook/ABookFileStore.ts
@@ -5,14 +5,23 @@ import { PrefixObjectFileStore } from "tws-common/file/ofs/ObjectFileStore"
 import LocalForageKeyValueStore from "tws-common/keyvalue/LocalForageKeyValueStore"
 import { GLOBAL_WEB_KEYED_LOCKS } from "tws-common/lang/lock/keyed/WebKeyedLocks"
 import KeyValueWALStore from "tws-common/lang/wal/KeyValueWALStore"
+import { claimId, NS_STORE, NS_WEB_LOCK } from "tws-common/misc/GlobalIDManager"
 
 export const ABOOK_FILE_STORE: PrefixObjectFileStore<ABookFileMetadata> =
 	new KeyValueObjectFileStore<ABookFileMetadata>(
-		LocalForageKeyValueStore.simple("abookFiles"),
-		LocalForageKeyValueStore.simple("abookFilesMetadata"),
-		new KeyValueWALStore(LocalForageKeyValueStore.simple("abookFilesWAL")),
+		LocalForageKeyValueStore.simple(
+			claimId(NS_STORE, "palm-abooks-pwa/abook-files"),
+		),
+		LocalForageKeyValueStore.simple(
+			claimId(NS_STORE, "palm-abooks-pwa/abook-files-metadata"),
+		),
+		new KeyValueWALStore(
+			LocalForageKeyValueStore.simple(
+				claimId(NS_STORE, "palm-abooks-pwa/abook-files-wal"),
+			),
+		),
 		GLOBAL_WEB_KEYED_LOCKS.getRWLockAdapter(
-			"app/palm-abooks-pwa/ABOOK_FILE_STORE",
+			claimId(NS_WEB_LOCK, "palm-abooks-pwa/abook-files-lock"),
 		),
 	)
 
