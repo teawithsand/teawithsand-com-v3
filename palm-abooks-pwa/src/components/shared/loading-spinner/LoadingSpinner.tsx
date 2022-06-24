e39bacc19b9f141d43f21e1b12024b4b17b67fab
teawithsand <teawithsand@gmail.com>
2022-06-24 16:49:32 +0000

    Ported LoadingSpinner to styled-components

--- palm-abooks-pwa/src/components/shared/loading-spinner/LoadingSpinner.tsx
+++ palm-abooks-pwa/src/components/shared/loading-spinner/LoadingSpinner.tsx
@@ -1,19 +1,44 @@
 import React from "react"
+import styled, { keyframes } from "styled-components"
 
-import * as styles from "./loadingSpinner.module.scss"
 import LoadingImage from "./loading.svg"
+
+const spinAnimation = keyframes`
+from {
+	transform: rotate(0deg);
+}
+to {
+	transform: rotate(360deg);
+}
+`
+
+const SpinnerContainer = styled.div`
+	margin-left: auto;
+	margin-right: auto;
+	width: fit-content;
+`
+
+const spinnerSize = 150
+const spinnerMargin = (spinnerSize * (Math.sqrt(2) - 1)) / 2
+
+const SpinnerImage = styled(LoadingImage)`
+	box-sizing: border-box;
+	width: ${spinnerSize}px;
+	height: ${spinnerSize}px;
+	animation: ${spinAnimation} 2s cubic-bezier(0.1, 0.7, 1, 0.1) infinite;
+	margin: ${spinnerMargin}px;
+`
+
+const SpinnerText = styled.div`
+	text-align: center;
+	font-size: 1.5em;
+`
 
 export default () => {
 	return (
-		<div className={styles.spinner}>
-			<LoadingImage
-				width={150}
-				height={150}
-				alt={"Loading..."}
-				src="./loading.svg"
-				className={styles.spinnerImage}
-			/>
-			<div className={styles.spinnerText}>Loading...</div>
-		</div>
+		<SpinnerContainer>
+			<SpinnerImage alt={"Loading..."} />
+			<SpinnerText>Loading...</SpinnerText>
+		</SpinnerContainer>
 	)
 }
