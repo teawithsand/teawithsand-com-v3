a695d4f34692decee41738921ea66a1e761ccfd4
teawithsand <teawithsand@gmail.com>
2022-06-26 16:05:22 +0000

    Added useConfirm hook

---
+++ tws-common/src/react/hook/useConfirm.ts
@@ -0,0 +1,36 @@
+import React, { useContext } from "react"
+import { latePromise } from "tws-common/lang/latePromise"
+
+export type ConfirmRequester<T> = (
+	data: T,
+	resolve: (res: boolean) => void,
+) => void
+
+export type ConfirmReactContext<T> = React.Context<{
+	requestConfirmation: ConfirmRequester<T>
+}>
+
+export const createConfirmContext = <T>(
+	requestConfirmation: ConfirmRequester<T>,
+): ConfirmReactContext<T> =>
+	React.createContext({
+		requestConfirmation,
+	})
+
+export type ConfirmationRequester<T> = (requestData: T) => Promise<boolean>
+
+/**
+ * Note: user may want to use some single type for
+ */
+export const useConfirm = <T>(
+	ctx: ConfirmReactContext<T>,
+): ConfirmationRequester<T> => {
+	return async (data: T) => {
+		const [p, resolve] = latePromise<boolean>()
+
+		const res = useContext(ctx)
+
+		res.requestConfirmation(data, resolve)
+		return await p
+	}
+}
