2b61919838954fae457d1a69e655eb10b22ee36b
teawithsand <teawithsand@gmail.com>
2022-06-28 07:19:26 +0000

    Implemented returning generic type from GTaskRunner returning interface, but generic type of context, which extends that interface

--- tws-common/src/misc/gtask/react.ts
+++ tws-common/src/misc/gtask/react.ts
@@ -1,9 +1,10 @@
 import React, { useContext } from "react"
 import { GTaskRunner } from "tws-common/misc/gtask/impl"
 
-export const createGTaskRunnerContext = <M>(runner: GTaskRunner<M>) =>
-	React.createContext(runner)
+export const createGTaskRunnerContext = <T extends GTaskRunner<M>, M>(
+	runner: T,
+): React.Context<T> => React.createContext(runner)
 
-export const useGTaskRunnerContext = <M>(
-	ctx: React.Context<GTaskRunner<M>>,
-): GTaskRunner<M> => useContext(ctx)
+export const useGTaskRunnerContext = <T extends GTaskRunner<M>, M>(
+	ctx: React.Context<T>,
+): T => useContext(ctx)
