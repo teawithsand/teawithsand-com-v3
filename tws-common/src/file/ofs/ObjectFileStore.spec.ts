71b23a0880b41dddb73091a516ae9370807080c0
teawithsand <teawithsand@gmail.com>
2022-06-29 16:35:53 +0000

    Added testing for KeyValueObjectFileStore for entry deletion

--- tws-common/src/file/ofs/ObjectFileStore.spec.ts
+++ tws-common/src/file/ofs/ObjectFileStore.spec.ts
@@ -2,7 +2,7 @@ import KeyValueObjectFileStore from "tws-common/file/ofs/KeyValueObjectFileStore
 import ObjectFileStore from "tws-common/file/ofs/ObjectFileStore"
 import InMemoryKeyValueStore from "tws-common/keyvalue/InMemoryKeyValueStore"
 import { arrayBufferFromBytes } from "tws-common/lang/buffer"
-import { FakeRWLockAdapter, NoopRWLockAdapter } from "tws-common/lang/lock/Lock"
+import { FakeRWLockAdapter } from "tws-common/lang/lock/Lock"
 import { MutexLockAdapter } from "tws-common/lang/lock/MutexLockAdapter"
 import KeyValueWALStore from "tws-common/lang/wal/KeyValueWALStore"
 
@@ -29,6 +29,20 @@ describe("KeyValueObjectFileStore", () => {
 		expect(file?.innerObject).toEqual(blob)
 		const metadata = await store.getMetadata("/asdf")
 		expect(metadata?.meaning).toStrictEqual("of life")
+	})
+
+	it("can delete entry", async () => {
+		await store.setFile("/asdf", new File([], "asdf.txt"), {
+			meaning: "of life",
+		})
+
+		await store.delete("/asdf")
+
+		const metadata = await store.getMetadata("/asdf")
+		expect(metadata).toBeNull()
+
+		const file = await store.getFile("/asdf")
+		expect(file).toBeNull()
 	})
 
 	it("can update metadata", async () => {
