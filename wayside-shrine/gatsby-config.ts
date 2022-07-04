babc8e453e2b3be80e0ffb4c745013e90b0f0f29
teawithsand <teawithsand@gmail.com>
2022-07-04 13:33:22 +0000

    Ported wayside-shrines config to use new gatsby plugin

--- wayside-shrine/gatsby-config.ts
+++ wayside-shrine/gatsby-config.ts
@@ -1,69 +1,20 @@
-import type { GatsbyConfig } from "gatsby"
-import * as path from "path"
+import {
+	GatsbyTransformerRemarkPlugins,
+	customizeDefaultPlugins,
+	makeManifestPlugin,
+	makeLayoutPlugin,
+	makeConfig,
+} from "tws-gatsby-plugin"
 
-const config: GatsbyConfig = {
-	siteMetadata: {
-		title: `wayside-shrine`,
-		siteUrl: `https://szlakiemkapliczek.pl`,
-	},
-	// More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-	// If you use VSCode you can also use the GraphQL plugin
-	// Learn more at: https://gatsby.dev/graphql-typegen
-	graphqlTypegen: true,
-	plugins: [
-		{
-			resolve: `gatsby-plugin-styled-components`,
-			options: {
-				displayName: false,
-				fileName: false,
-			},
-		},
-		"gatsby-plugin-sass",
-		"gatsby-plugin-image",
-		"gatsby-plugin-react-helmet",
-		"gatsby-plugin-sitemap",
-		{
-			resolve: "gatsby-plugin-manifest",
-			options: {
-				icon: "./src/images/icon.png",
-			},
-		},
-		{
-			resolve: `gatsby-transformer-remark`,
-			options: {
-				// extensions: ['.md', '.mdx'],
-				plugins: [
-					{
-						resolve: `gatsby-remark-images`,
-						options: {
-							srcSetBreakpoints: [
-								1920 / 8,
-								1920 / 4,
-								1920 / 2,
-								1920,
-							],
-							withWebp: {
-								quality: 80,
-							},
-							withAvif: {
-								quality: 80,
-							},
-							tracedSVG: true,
-						},
-					},
-					{
-						resolve: `gatsby-remark-responsive-iframe`,
-						options: {
-							wrapperStyle: `margin-bottom: 1.0725rem`,
-						},
-					},
-					`gatsby-remark-copy-linked-files`,
-					`gatsby-remark-smartypants`,
-				],
-			},
-		},
-		"gatsby-plugin-sharp",
-		"gatsby-transformer-sharp",
+import * as fs from "fs"
+
+const plugins = customizeDefaultPlugins(
+	[
+		makeManifestPlugin("./src/images/icon.png"),
+		makeLayoutPlugin("./src/Layout.jsx"),
+	],
+	GatsbyTransformerRemarkPlugins,
+	[
 		{
 			resolve: "gatsby-source-filesystem",
 			options: {
@@ -87,24 +38,21 @@ const config: GatsbyConfig = {
 			},
 			__key: "pages",
 		},
-		{
-			resolve: "gatsby-plugin-layout",
-			options: {
-				component: path.resolve(`./src/Layout.jsx`),
-			},
-		},
-		{
-			resolve: "gatsby-plugin-zopfli",
-			options: {
-				extensions: ["css", "html", "js", "svg", "txt", "json"],
-				compression: {
-					numiterations: 15,
-					blocksplitting: true,
-					blocksplittingmax: 15,
-				},
-			},
-		},
 	],
-}
+)
+// JIC debugging is here
+/*
+fs.writeFileSync(
+	"/workspaces/tws_blog/wayside-shrine/debuginfo.json",
+	JSON.stringify(plugins),
+)
+*/
+const config = makeConfig(
+	{
+		title: `wayside-shrine`,
+		siteUrl: `https://szlakiemkapliczek.pl`,
+	},
+	plugins,
+)
 
 export default config
