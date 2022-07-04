0f1cacaf87139cb1ec0738d4c533e22565bd697f
teawithsand <teawithsand@gmail.com>
2022-07-04 10:48:29 +0000

    Added notice that one could provide better anti-ssr protection

--- tws-common/src/ssr/index.ts
+++ tws-common/src/ssr/index.ts
@@ -7,5 +7,8 @@ export const canUseDom = () => innerCanUseDom
 export const isSSR = () => !canUseDom()
 
 export const requireNoSSR = () => {
-	if (!canUseDom()) throw new Error("This function does not work with SSR")
+	if (!canUseDom()) {
+		throw new Error("This function does not work with SSR")
+		// TODO(teawithsand): here we should have access to node APIs, so we can write file with note or sth
+	}
 }
