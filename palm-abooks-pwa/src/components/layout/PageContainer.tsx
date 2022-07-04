722cf063b64bca63cb2833df8732978e64774315
teawithsand <teawithsand@gmail.com>
2022-07-04 14:08:30 +0000

    Refactored page container, since ReactNode is also ReactFragment

--- palm-abooks-pwa/src/components/layout/PageContainer.tsx
+++ palm-abooks-pwa/src/components/layout/PageContainer.tsx
@@ -1,8 +1,8 @@
-import React, { ReactFragment, ReactNode } from "react"
+import React, { ReactNode } from "react"
 
 import { Container as ParentContainer } from "tws-common/ui"
 
-const PageContainer = (props: { children?: ReactFragment | ReactNode }) => {
+const PageContainer = (props: { children?: ReactNode }) => {
 	return <ParentContainer className="mt-5">{props.children}</ParentContainer>
 }
 export default PageContainer
