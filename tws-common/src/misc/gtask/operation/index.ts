51256480341ccaf85503b443256df236aa525b29
teawithsand <teawithsand@gmail.com>
2022-07-10 18:55:30 +0000

    Added tools for using operations component along with gtask runner

--- tws-common/src/misc/gtask/operation/index.ts
+++ tws-common/src/misc/gtask/operation/index.ts
@@ -1,4 +1,5 @@
-import { useMutation } from "react-query"
+import { MutationKey, useMutation } from "react-query"
+import { GTaskRunner } from "tws-common/misc/gtask/impl"
 import { Operation } from "tws-common/misc/gtask/operation/action"
 
 export * from "./action"
@@ -17,12 +18,71 @@ export const useOperationExecutor = <C, D, R>(
  * Gives utils for tracking task's state and more.
  */
 export const useOperation = <C, D, R>(
+	key: MutationKey,
 	config: C,
 	operation: Operation<C, D, R>,
 ) => {
 	const executor = operation(config)
 
-	const mutation = useMutation(async (data: D) => await executor(data))
+	const mutation = useMutation(key, async (data: D) => await executor(data))
+
+	return {
+		run: mutation.mutateAsync,
+		...mutation,
+	}
+}
+
+export const useOperationOnGTaskWithMetadata = <
+	C,
+	D,
+	R,
+	T extends GTaskRunner<M>,
+	M,
+>(
+	key: MutationKey,
+	runner: T,
+	config: C,
+	metadata: M,
+	operation: Operation<C, D, R>,
+) => {
+	const executor = useOperationExecutor(config, operation)
+
+	const mutation = useMutation(key, async (data: D) => {
+		const handle = runner.putTask({
+			metadata: metadata,
+			task: async () => {
+				return await executor(data)
+			},
+		})
+
+		await handle.promise
+	})
+
+	return {
+		run: mutation.mutateAsync,
+		...mutation,
+	}
+}
+
+export const useOperationOnGTask = <C, D, R, T extends GTaskRunner<M>, M>(
+	key: MutationKey,
+	runner: T,
+	config: C,
+	operation: Operation<C, D, R>,
+) => {
+	const executor = useOperationExecutor(config, operation)
+
+	const mutation = useMutation(key, async (arg: { metadata: M; data: D }) => {
+		const { metadata, data } = arg
+		const handle = runner.putTask({
+			metadata: metadata,
+			task: async () => {
+				return await executor(data)
+			},
+		})
+
+		await handle.promise
+	})
 
 	return {
 		run: mutation.mutateAsync,
