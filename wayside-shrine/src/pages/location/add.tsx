891d3b3fe37c4644fe7f9e9a01a424f59a4472eb
teawithsand <teawithsand@gmail.com>
2022-07-10 17:23:05 +0000

    Implemented example modal on add.tsx page

--- wayside-shrine/src/pages/location/add.tsx
+++ wayside-shrine/src/pages/location/add.tsx
@@ -1,14 +1,40 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import PageContainer from "@app/components/layout/PageContainer"
-import LocationList from "@app/components/location/LocationList"
+
+import { Button, Modal } from "tws-common/ui"
+import { useDialogManager } from "tws-common/ui/dialog"
 
 const LocationAddPage = () => {
+	const dm = useDialogManager()
+
+	useEffect(() => {
+		dm.showDialog<void>(({ resolve }) => {
+			const onClose = () => {
+				resolve()
+			}
+			return (
+				<Modal show={true} size="lg" fullscreen="lg" onHide={onClose}>
+					<Modal.Header closeButton></Modal.Header>
+					<Modal.Body>Ima modal man</Modal.Body>
+					<Modal.Footer>
+						<Button variant="secondary" onClick={onClose}>
+							Close
+						</Button>
+						<Button variant="primary" onClick={onClose}>
+							Save Changes
+						</Button>
+					</Modal.Footer>
+				</Modal>
+			)
+		})
+			.then(console.log)
+			.catch(console.error)
+	}, [])
+
 	return (
 		<PageContainer>
-			<main>
-				NIY
-			</main>
+			<main>NIY</main>
 		</PageContainer>
 	)
 }
