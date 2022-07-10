f0bd11a37d95a27d40e40457152ce1c3b119a1ff
teawithsand <teawithsand@gmail.com>
2022-07-10 19:14:21 +0000

    Implemented simple yet useful error bar component, which is opinionated but nice

---
+++ tws-common/src/ui/ErrorBar.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode, useMemo } from "react"
+import { Alert } from "react-bootstrap"
+import { ErrorExplainer } from "tws-common/misc/error-explainer"
+
+/**
+ * Simple error bar, which shows up only if error is truthy.
+ * Uses error explainer to display error details if there is any error.
+ */
+export const ErrorBar = <T extends ReactNode>(props: {
+	explainer: ErrorExplainer<T>
+	error: any
+}) => {
+	const { explainer, error } = props
+
+	const explained = useMemo(() => {
+		if (error) {
+			return explainer(error)
+		}
+		return null
+	}, [error])
+
+	if (!error) {
+		return null
+	}
+
+	return <Alert variant="danger">{explained}</Alert>
+}
