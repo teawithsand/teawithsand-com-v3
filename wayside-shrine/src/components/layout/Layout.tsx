f9edecb5d30b2c8f6bfdd3f1097b40c32bd17af6
teawithsand <teawithsand@gmail.com>
2022-07-11 19:04:02 +0000

    Removed old unused boundaries from layout

--- wayside-shrine/src/components/layout/Layout.tsx
+++ wayside-shrine/src/components/layout/Layout.tsx
@@ -1,14 +1,10 @@
 import React, { ReactElement, ReactFragment, ReactNode } from "react"
 
-import ErrorRenderer from "@app/components/layout/ErrorRenderer"
 import AppNavbar from "@app/components/layout/Navbar"
 
 import { GlobalIdManager } from "tws-common/misc/GlobalIDManager"
 import { DialogBoundary } from "tws-common/react/components/dialog"
-import { ErrorWall } from "tws-common/react/components/error-wall"
-import { SimpleSuspenseDiv } from "tws-common/react/components/simple-suspense"
 import { QueryClient, QueryClientProvider } from "tws-common/react/hook/query"
-import LoadingSpinner from "tws-common/ui/LoadingSpinner"
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -28,16 +24,10 @@ const Layout = (props: {
 	children: ReactElement | ReactNode | ReactFragment | null | undefined
 }) => {
 	return (
-		<>
-			<QueryClientProvider client={queryClient}>
-				<AppNavbar />
-				<SimpleSuspenseDiv fallback={() => <LoadingSpinner />}>
-					<ErrorWall errorRenderer={ErrorRenderer}>
-						<DialogBoundary>{props.children}</DialogBoundary>
-					</ErrorWall>
-				</SimpleSuspenseDiv>
-			</QueryClientProvider>
-		</>
+		<QueryClientProvider client={queryClient}>
+			<AppNavbar />
+			<DialogBoundary>{props.children}</DialogBoundary>
+		</QueryClientProvider>
 	)
 }
 
