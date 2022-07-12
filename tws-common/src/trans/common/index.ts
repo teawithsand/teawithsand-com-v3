f6f9fd909dfb24b0e517581e9375f39412328373
teawithsand <teawithsand@gmail.com>
2022-07-12 16:05:03 +0000

    Added set of common translations

---
+++ tws-common/src/trans/common/index.ts
@@ -0,0 +1,53 @@
+import { TimestampMs } from "tws-common/lang/time/Timestamp"
+
+export type TranslatableDate = Date | TimestampMs
+
+export const translatableDateToDate = (date: TranslatableDate) => {
+	if (date instanceof Date) return date
+	return new Date(date)
+}
+
+/**
+ * Predefined set of translations, which is commonly used so was migrated to tws-common.
+ */
+export interface CommonTranslation {
+	// language in format like en-US or en-GB
+	language: {
+		twoPartCode: string
+		singlePartCode: string
+		languageName: string
+	}
+
+	// Date only
+	formatDate: (date: TranslatableDate) => string
+	// Date + Time
+	formatDateTime: (date: TranslatableDate) => string
+	// Time only
+	formatTime: (date: TranslatableDate) => string
+
+	dialog: {
+		areYouSure: {
+			defaultTitle: string
+			defaultMessage: string
+			defaultConfirmLabel: string
+			defaultCancelLabel: string
+
+            messageDelete: string
+		}
+	}
+}
+
+export const makeCommonTranslationTemplate = (
+	language: CommonTranslation["language"],
+) => ({
+	language,
+	formatDate: (date: TranslatableDate) =>
+		translatableDateToDate(date).toLocaleDateString(language.twoPartCode),
+	formatDateTime: (date: TranslatableDate) =>
+		translatableDateToDate(date).toLocaleString(language.twoPartCode),
+	formatTime: (date: TranslatableDate) =>
+		translatableDateToDate(date).toLocaleTimeString(language.twoPartCode),
+})
+
+export * from "./en-us"
+export * from "./pl-pl"
