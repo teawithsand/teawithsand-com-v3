c6ecd2870b82b94ab694dc9256f1673e2545bbd0
teawithsand <teawithsand@gmail.com>
2022-07-13 15:53:24 +0000

    Added docs on useIsSSR

--- tws-common/src/react/hook/isSSR.ts
+++ tws-common/src/react/hook/isSSR.ts
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react"
 
+/**
+ * Returns true, if SSR is enabled in SSR-capable manner.
+ */
 export const useIsSSR = () => {
 	const [isServer, setIsServer] = useState(true)
 	useEffect(() => {
