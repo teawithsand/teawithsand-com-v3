4615656a2fddd5d0c1cb5bb60c98e41a9bf37007
teawithsand <teawithsand@gmail.com>
2022-07-13 15:23:53 +0000

    Wrapped list locations page in no-ssr

--- wayside-shrine/src/pages/location/list.tsx
+++ wayside-shrine/src/pages/location/list.tsx
@@ -2,8 +2,10 @@ import React from "react"
 
 import LocationListPage from "@app/components/page/LocationListPage"
 
+import { wrapNoSSR } from "tws-common/react/components/NoSSR"
+
 const Page = () => {
 	return <LocationListPage />
 }
 
-export default Page
+export default wrapNoSSR(Page)
