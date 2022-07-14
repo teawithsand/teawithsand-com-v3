d15bdf33cc511cf81150dcb7a727b149332bf6ea
teawithsand <teawithsand@gmail.com>
2022-07-14 20:26:44 +0000

    Implemented manual add location page

--- wayside-shrine/src/components/page/AddLocationPage.tsx
+++ wayside-shrine/src/components/page/AddLocationPage.tsx
@@ -4,6 +4,7 @@ import PageBoundary from "@app/components/layout/PageBoundary"
 import PageContainer from "@app/components/layout/PageContainer"
 import { LocationForm } from "@app/components/location/form/LocationForm"
 import { useUpsertLocation } from "@app/domain/location/operation"
+import { locationShowPath } from "@app/paths"
 
 import { getNowTimestamp } from "tws-common/lang/time/Timestamp"
 import { generateUUID } from "tws-common/lang/uuid"
@@ -16,27 +17,29 @@ const AddLocationPage = () => {
 
 	return (
 		<PageContainer>
-			<PageBoundary>
-				<LocationForm
-					onSubmit={async data => {
-						const id = generateUUID()
-						await mutation.mutateAsync({
-							id,
-							data: {
-								coordinates: {
-									latitude: data.latitude,
-									longitude: data.longitude,
+			<main>
+				<PageBoundary>
+					<LocationForm
+						onSubmit={async data => {
+							const id = generateUUID()
+							await mutation.mutateAsync({
+								id,
+								data: {
+									coordinates: {
+										latitude: data.latitude,
+										longitude: data.longitude,
+									},
+									description: data.description,
+									name: data.name,
+									timestamp: getNowTimestamp(),
 								},
-								description: data.description,
-								name: data.name,
-								timestamp: getNowTimestamp(),
-							},
-						})
+							})
 
-						// TODO(teawithsand): here use navigate to show location here
-					}}
-				/>
-			</PageBoundary>
+							navigate(locationShowPath(id))
+						}}
+					/>
+				</PageBoundary>
+			</main>
 		</PageContainer>
 	)
 }
