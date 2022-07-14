33d1a059844c23d30d6831bbab7ffeb2cb4d0122
teawithsand <teawithsand@gmail.com>
2022-07-14 20:46:40 +0000

    Added soft require-no-ssr notice

--- tws-common/src/react/hook/useUniqueId.ts
+++ tws-common/src/react/hook/useUniqueId.ts
@@ -1,7 +1,11 @@
 import { useMemo } from "react"
 import { generateUUID } from "tws-common/lang/uuid"
 
+/**
+ * This hook SHOULD NOT be used with SSR, since it may cause different results when rendering on server and on client.
+ */
 const useUniqueId = () => {
+	// requireNoSSR() // may break some code for now, but should be included in future
 	return useMemo(() => generateUUID(), [])
 }
 
