935c5df6e582347eef030551b3dfaac2f5590480
teawithsand <teawithsand@gmail.com>
2022-07-20 20:01:54 +0000

    Changed margin of PageContainer to smaller one

--- tws-blog/src/components/layout/PageContainer.tsx
+++ tws-blog/src/components/layout/PageContainer.tsx
@@ -3,6 +3,6 @@ import React, { ReactFragment, ReactNode } from "react"
 import { Container as ParentContainer } from "tws-common/ui"
 
 const PageContainer = (props: { children?: ReactFragment | ReactNode }) => {
-	return <ParentContainer className="mt-5">{props.children}</ParentContainer>
+	return <ParentContainer className="mt-3">{props.children}</ParentContainer>
 }
 export default PageContainer
