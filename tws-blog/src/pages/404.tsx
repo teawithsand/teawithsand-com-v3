3f89eac1dc6be35916608c4e2eaa82be87414b2f
teawithsand <teawithsand@gmail.com>
2022-07-22 18:13:06 +0000

    Improved 404 page

--- tws-blog/src/pages/404.tsx
+++ tws-blog/src/pages/404.tsx
@@ -1,34 +1,20 @@
-import * as React from "react"
+import PageContainer from "@app/components/layout/PageContainer"
+import { homePath } from "@app/paths"
 import { Link } from "gatsby"
+import * as React from "react"
 
-// styles
-const pageStyles = {
-	color: "#232129",
-	padding: "96px",
-	fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-	marginTop: 0,
-	marginBottom: 64,
-	maxWidth: 320,
-}
-
-const paragraphStyles = {
-	marginBottom: 48,
-}
-const codeStyles = {
-	color: "#8A6534",
-	padding: 4,
-	backgroundColor: "#FFF4DB",
-	fontSize: "1.25rem",
-	borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
 	return (
-		<main style={pageStyles}>
-			<h1 style={headingStyles}>Page not found</h1>
+		<main>
+			<PageContainer>
+				<h1
+					style={{
+						textAlign: "center",
+					}}
+				>
+					<Link to={homePath}>Page not found - go to home page</Link>
+				</h1>
+			</PageContainer>
 		</main>
 	)
 }
