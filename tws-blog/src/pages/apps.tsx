fa1d37360f88ec113d5efc01e9c2ba038d547ea7
teawithsand <teawithsand@gmail.com>
2022-07-22 18:39:09 +0000

    Added NIY apps page

---
+++ tws-blog/src/pages/apps.tsx
@@ -0,0 +1,8 @@
+import PageContainer from "@app/components/layout/PageContainer"
+import * as React from "react"
+
+const AppsPage = () => {
+	return <PageContainer>NIY</PageContainer>
+}
+
+export default AppsPage
