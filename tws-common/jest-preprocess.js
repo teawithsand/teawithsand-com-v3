712ecb211a7403509b91adbeacad45f5727a7bf2
teawithsand <teawithsand@gmail.com>
2022-07-27 18:45:20 +0000

    Added react preset to tws-common testing babel config

--- tws-common/jest-preprocess.js
+++ tws-common/jest-preprocess.js
@@ -1,4 +1,4 @@
 const babelOptions = {
-	presets: ["@babel/preset-env", "@babel/preset-typescript"],
+	presets: ["@babel/preset-env", "@babel/preset-typescript", "@babel/preset-react"],
 }
 module.exports = require("babel-jest").default.createTransformer(babelOptions)
