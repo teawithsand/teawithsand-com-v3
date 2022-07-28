94156013c3257b9b95e23bc47cff52eee980987b
teawithsand <teawithsand@gmail.com>
2022-07-28 20:55:41 +0000

    Added fancy tile background to top-level paint element

--- tws-paint/src/components/paint/Paint.tsx
+++ tws-paint/src/components/paint/Paint.tsx
@@ -1,7 +1,10 @@
-import React from "react"
+import React, { FC } from "react"
 import styled from "styled-components"
 
 import { SidePanel } from "@app/components/paint/side-panel/SidePanel"
+import { PaintScene } from "@app/domain/paint/defines"
+
+export type SceneRenderer = FC<PaintScene>
 
 const InnerContainer = styled.div`
 	display: grid;
@@ -11,12 +14,22 @@ const InnerContainer = styled.div`
 	padding: 0;
 	margin: 0;
 
-    overflow: hidden;
+	overflow: hidden;
 
 	& > * {
 		grid-row: 1;
 		grid-column: 1;
 	}
+
+	background-image: linear-gradient(45deg, #808080 25%, transparent 25%),
+		linear-gradient(-45deg, #808080 25%, transparent 25%),
+		linear-gradient(45deg, transparent 75%, #808080 75%),
+		linear-gradient(-45deg, transparent 75%, #808080 75%);
+	background-size: 20px 20px;
+	background-position: 0 0, 0 10px, 10px -10px, -10px 0px;
+
+	background: repeating-conic-gradient(#808080 0% 25%, transparent 0% 50%) 50% /
+		20px 20px;
 `
 
 export const Paint = () => {
